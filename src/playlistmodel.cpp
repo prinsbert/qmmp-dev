fc75f54746b1af7e78f5ecf941ec39e024a5c1d5
trialuser02 <trialuser02>
2007-08-24 13:40:23 +0000

    added current song saving
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.1@125 90c681e8-e032-0410-971d-27865f9a5e38

--- src/playlistmodel.cpp
+++ src/playlistmodel.cpp
@@ -26,6 +26,7 @@
 #include <QPluginLoader>
 #include <QApplication>
 #include <QTimer>
+#include <QSettings>
 
 #include <time.h>
 
@@ -102,7 +103,7 @@ MediaFile* PlayListModel::currentItem()
     if ( m_files.isEmpty() )
         return 0;
     else
-        return m_files.at ( m_current );
+        return m_files.at ( qMin(m_files.size() - 1, m_current));
 }
 
 int PlayListModel::currentRow()
@@ -188,16 +189,16 @@ QList <QString> PlayListModel::getTimes ( int b,int l )
 
 bool PlayListModel::isSelected ( int row )
 {
-	if(m_files.count() > row && row >= 0)
-      return m_files.at ( row )->isSelected();
-	
-	return false;
+    if (m_files.count() > row && row >= 0)
+        return m_files.at ( row )->isSelected();
+
+    return false;
 }
 
 void PlayListModel::setSelected ( int row, bool yes )
 {
-	if(m_files.count() > row && row >= 0)
-    	m_files.at ( row )->setSelected ( yes );
+    if (m_files.count() > row && row >= 0)
+        m_files.at ( row )->setSelected ( yes );
 }
 
 void PlayListModel::removeSelected()
@@ -238,7 +239,7 @@ void PlayListModel::removeSelection(bool inverted)
     if (!m_files.isEmpty())
         m_currentItem = m_files.at(m_current);
 
-    if(select_after_delete >= m_files.count())
+    if (select_after_delete >= m_files.count())
         select_after_delete = m_files.count() - 1;
 
     setSelected(select_after_delete,true);
@@ -268,7 +269,7 @@ void PlayListModel::showDetails()
     {
         if ( m_files.at ( i )->isSelected() )
         {
-            DecoderFactory *fact = Decoder::findFactory ( m_files.at ( i )->path() );
+            DecoderFactory *fact = Decoder::findByPath ( m_files.at ( i )->path() );
             if ( fact )
                 fact->showDetails ( 0, m_files.at ( i )->path() );
 
@@ -281,6 +282,9 @@ void PlayListModel::showDetails()
 
 void PlayListModel::readSettings()
 {
+    QSettings settings(QDir::homePath()+"/.qmmp/qmmprc", QSettings::IniFormat);
+    m_current = settings.value("Playlist/current",0).toInt();
+
     QFile file ( QDir::homePath() +"/.qmmp/playlist.txt" );
     file.open ( QIODevice::ReadOnly );
 
@@ -296,18 +300,21 @@ void PlayListModel::readSettings()
 
     file.close ();
 
+    if(m_current > files.count() - 1)
+        m_current = 0;
+
     int preload = (files.count() < 100) ? files.count() : 100;
 
     for (int i = 0;i < preload;i++)
-	 {
+    {
         load(new MediaFile(files.takeAt(0)));
-	 }
+    }
 
 
     if (files.isEmpty())
-       return;
+        return;
 
-	 FileLoader* f_loader = createFileLoader();
+    FileLoader* f_loader = createFileLoader();
 
     f_loader->setFilesToLoad(files);
     //f_loader->start(QThread::IdlePriority);
@@ -322,13 +329,17 @@ void PlayListModel::writeSettings()
     foreach ( MediaFile* m, m_files )
     file.write ( m->path().toUtf8 () +"\n" );
     file.close ();
+    QSettings settings(QDir::homePath()+"/.qmmp/qmmprc", QSettings::IniFormat);
+    settings.setValue("Playlist/current", m_current);
 }
 
 void PlayListModel::addFile(const QString& path)
 {
     if (path.isEmpty ())
         return;
-    if (Decoder::supports(path))
+    if (path.startsWith("http://"))
+        load(new MediaFile(path));
+    else if (Decoder::supports(path))
         load(new MediaFile(path));
 
     m_play_state->prepare();
@@ -359,6 +370,24 @@ void PlayListModel::addDirectory(const QString& s)
     f_loader->start(QThread::IdlePriority);
 }
 
+void PlayListModel::addFileList(const QStringList &l)
+{
+//    qWarning("void// PlayListModel::addFileList(const QStringList &l)");
+    foreach(QString str,l)
+    {
+        QFileInfo f_info(str);
+        if (f_info.exists())
+        {
+            if (f_info.isDir())
+                addDirectory(str);
+            else
+                addFile(str);
+        }
+        // Do processing the rest of events to avoid GUI freezing
+        QApplication::processEvents(QEventLoop::AllEvents,10);
+    }
+}
+
 bool PlayListModel::setFileList(const QStringList & l)
 {
     bool model_cleared = FALSE;
@@ -377,7 +406,7 @@ bool PlayListModel::setFileList(const QStringList & l)
             else
                 addFile(str);
         }
-        // Do the processing the rest of events to avoid GUI freezing
+        // Do processing the rest of events to avoid GUI freezing
         QApplication::processEvents(QEventLoop::AllEvents,10);
     }
 
@@ -508,24 +537,24 @@ void PlayListModel::addToQueue()
     QList<MediaFile*> selected_items = getSelectedItems();
     foreach(MediaFile* file,selected_items)
     {/*
-        if (isQueued(file))
-            m_queued_songs.removeAt(m_queued_songs.indexOf(file));
-        else
-            m_queued_songs.append(file);
-		 */
-		 setQueued(file);
+                if (isQueued(file))
+                    m_queued_songs.removeAt(m_queued_songs.indexOf(file));
+                else
+                    m_queued_songs.append(file);
+                 */
+        setQueued(file);
     }
     emit listChanged();
 }
 
 void PlayListModel::setQueued(MediaFile* file)
 {
-	if (isQueued(file))
-		m_queued_songs.removeAt(m_queued_songs.indexOf(file));
-	else
-		m_queued_songs.append(file);
-	
-	emit listChanged();
+    if (isQueued(file))
+        m_queued_songs.removeAt(m_queued_songs.indexOf(file));
+    else
+        m_queued_songs.append(file);
+
+    emit listChanged();
 }
 
 bool PlayListModel::isQueued(MediaFile* f) const
@@ -761,9 +790,9 @@ void PlayListModel::savePlaylist(const QString & f_name)
 
 void PlayListModel::loadExternalPlaylistFormats()
 {
-    QDir pluginsDir (qApp->applicationDirPath());
-    pluginsDir.cdUp();
-    pluginsDir.cd("Plugins/PlaylistFormats");
+    QDir pluginsDir (QDir::homePath()+"/.qmmp/plugins/PlaylistFormats");
+    //pluginsDir.cdUp();
+    //pluginsDir.cd("plugins/PlaylistFormats");
     foreach (QString fileName, pluginsDir.entryList(QDir::Files))
     {
         QPluginLoader loader(pluginsDir.absoluteFilePath(fileName));
