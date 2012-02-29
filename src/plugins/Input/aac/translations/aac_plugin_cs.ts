fd67002a9f04f93dd8e593cf6913fe8959775def
trialuser02 <trialuser02>
2012-02-29 17:06:04 +0000

    updated linguist files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.5@2657 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/aac/translations/aac_plugin_cs.ts
+++ src/plugins/Input/aac/translations/aac_plugin_cs.ts
@@ -42,27 +42,27 @@
 <context>
     <name>DecoderAACFactory</name>
     <message>
-        <location filename="../decoderaacfactory.cpp" line="45"/>
+        <location filename="../decoderaacfactory.cpp" line="46"/>
         <source>AAC Plugin</source>
         <translation>Modul AAC</translation>
     </message>
     <message>
-        <location filename="../decoderaacfactory.cpp" line="47"/>
+        <location filename="../decoderaacfactory.cpp" line="48"/>
         <source>AAC Files</source>
         <translation>Soubory AAC</translation>
     </message>
     <message>
-        <location filename="../decoderaacfactory.cpp" line="87"/>
+        <location filename="../decoderaacfactory.cpp" line="88"/>
         <source>About AAC Audio Plugin</source>
         <translation>O modulu AAC</translation>
     </message>
     <message>
-        <location filename="../decoderaacfactory.cpp" line="88"/>
+        <location filename="../decoderaacfactory.cpp" line="89"/>
         <source>Qmmp AAC Audio Plugin</source>
         <translation>Vstupní modul Qmmp AAC</translation>
     </message>
     <message>
-        <location filename="../decoderaacfactory.cpp" line="89"/>
+        <location filename="../decoderaacfactory.cpp" line="90"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
         <translation>Autor: Ilja Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
