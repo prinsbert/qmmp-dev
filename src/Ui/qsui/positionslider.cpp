33cb40feb9e66bc5ddcf8de9dfdb57300bdb3c97
trialuser02 <trialuser02>
2012-11-10 19:25:21 +0000

    qsui: fixed position bar behaviour (backported from trunk)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-plugin-pack-0.6@2981 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Ui/qsui/positionslider.cpp
+++ src/Ui/qsui/positionslider.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2011 by Ilya Kotov                                      *
+ *   Copyright (C) 2011-2012 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -15,7 +15,7 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         **
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 
 #include "positionslider.h"
@@ -31,10 +31,14 @@ void PositionSlider::mousePressEvent (QMouseEvent *event)
 		if (orientation() == Qt::Vertical)
 			setValue(minimum() + ((maximum()-minimum()) * (height()-event->y())) / height() ) ;
 		else
-			setValue(minimum() + ((maximum()-minimum()) * event->x()) / width() ) ;
-
+			setValue(minimum() + ((maximum()-minimum()) * event->x()) / width());
+		setSliderDown (true);
 		event->accept();
 	}
 	QSlider::mousePressEvent(event);
 }
 
+void PositionSlider::mouseReleaseEvent (QMouseEvent *)
+{
+	setSliderDown (false);
+}
