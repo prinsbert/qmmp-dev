3c7d93feb5c99f278035681dfbea853398aa5416
trialuser02 <trialuser02>
2012-12-15 17:13:08 +0000

    qsui: updated url dialog
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-plugin-pack-0.6@3070 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Ui/qsui/addurldialog.cpp
+++ src/Ui/qsui/addurldialog.cpp
@@ -15,7 +15,7 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         **
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 
 #include <QSettings>
@@ -24,7 +24,6 @@
 #include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QNetworkProxy>
-#include <QUrl>
 #include <QMessageBox>
 #include <QClipboard>
 #include <qmmpui/playlistparser.h>
@@ -119,12 +118,25 @@ void AddUrlDialog::accept( )
 
 void AddUrlDialog::readResponse(QNetworkReply *reply)
 {
-    addButton->setEnabled(true);
     disconnect(m_http, SIGNAL(finished (QNetworkReply *)), 0, 0);
     if (reply->error() != QNetworkReply::NoError)
         QMessageBox::critical (this, tr("Error"), reply->errorString ());
     else if (!urlComboBox->currentText().isEmpty())
     {
+        QUrl url = reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
+
+        if(!url.isEmpty() && url != m_redirect_url)
+        {
+            m_redirect_url = url;
+            connect(m_http, SIGNAL(finished (QNetworkReply *)), SLOT(readResponse(QNetworkReply *)));
+            QNetworkRequest request(url);
+            request.setRawHeader("User-Agent", QString("qmmp/%1").arg(Qmmp::strVersion()).toAscii());
+            m_http->get(request);
+            reply->deleteLater();
+            return;
+        }
+        m_redirect_url.clear();
+
         QString s = urlComboBox->currentText();
         PlaylistFormat* prs = PlaylistParser::instance()->findByPath(s);
         if (prs)
@@ -133,6 +145,7 @@ void AddUrlDialog::readResponse(QNetworkReply *reply)
             QDialog::accept();
         }
     }
+    addButton->setEnabled(true);
     reply->deleteLater();
 }
 
