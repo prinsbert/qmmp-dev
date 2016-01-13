dcf7a6103fddda700ad28cef1bd140f27965d684
trialuser02 <trialuser02>
2016-01-13 06:20:50 +0000

    updated .ts files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@6001 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Input/ffap/translations/ffap_plugin_sr_RS.ts
+++ qmmp-plugin-pack/src/Input/ffap/translations/ffap_plugin_sr_RS.ts
@@ -4,37 +4,37 @@
 <context>
     <name>DecoderFFapFactory</name>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="48"/>
+        <location filename="../decoderffapfactory.cpp" line="51"/>
         <source>FFap Plugin</source>
         <translation>ФФап прикључак</translation>
     </message>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="50"/>
+        <location filename="../decoderffapfactory.cpp" line="53"/>
         <source>Monkey&apos;s Audio Files</source>
         <translation>Манки аудио фајлови</translation>
     </message>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="153"/>
+        <location filename="../decoderffapfactory.cpp" line="161"/>
         <source>About FFap Audio Plugin</source>
         <translation>О ФФап прикључку</translation>
     </message>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="154"/>
+        <location filename="../decoderffapfactory.cpp" line="162"/>
         <source>Qmmp FFap Audio Plugin</source>
         <translation>Кумп ФФап прикључак</translation>
     </message>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="155"/>
+        <location filename="../decoderffapfactory.cpp" line="163"/>
         <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
         <translation>Подршка за Манки аудио (APE)</translation>
     </message>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="156"/>
+        <location filename="../decoderffapfactory.cpp" line="164"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
         <translation>Аутор: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
-        <location filename="../decoderffapfactory.cpp" line="157"/>
+        <location filename="../decoderffapfactory.cpp" line="165"/>
         <source>Based on code from deadbeef, FFmpeg and rockbox</source>
         <translation>Заснован на изворном кôду пројеката deadbeef, FFmpeg и rockbox</translation>
     </message>
