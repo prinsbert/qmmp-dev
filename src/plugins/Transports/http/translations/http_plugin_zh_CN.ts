95d64282625d4cacc203fb8af3766290e612b790
trialuser02 <trialuser02>
2016-01-25 07:24:38 +0000

    updated Chinese Simplified translation (Mingye Wang)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.9@6090 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Transports/http/translations/http_plugin_zh_CN.ts
+++ src/plugins/Transports/http/translations/http_plugin_zh_CN.ts
@@ -69,12 +69,12 @@
     <message>
         <location filename="../settingsdialog.ui" line="137"/>
         <source>User Agent:</source>
-        <translation type="unfinished"></translation>
+        <translation>用户代理：</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="151"/>
         <source>Change User Agent</source>
-        <translation type="unfinished"></translation>
+        <translation>更改用户代理</translation>
     </message>
 </context>
 </TS>
