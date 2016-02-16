dc2c0938f3cc622d8cc14acbb6b985b192eecd12
trialuser02 <trialuser02>
2016-02-16 08:28:36 +0000

    fixed previous commit
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.1@6138 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/ffmpeg/decoder_ffmpeg.cpp
+++ src/plugins/Input/ffmpeg/decoder_ffmpeg.cpp
@@ -91,10 +91,10 @@ DecoderFFmpeg::~DecoderFFmpeg()
     if (ic)
         avformat_free_context(ic);
     if(m_pkt.data)
-#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(104<<8)+102)) //ffmpeg-3.0
-        av_free_packet(&m_pkt);
-#else
+#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(24<<8)+102)) //ffmpeg-3.0
         av_packet_unref(&m_pkt);
+#else
+        av_free_packet(&m_pkt);
 #endif
     if(m_stream)
         av_free(m_stream);
@@ -375,10 +375,10 @@ qint64 DecoderFFmpeg::ffmpeg_decode()
         m_temp_pkt.size -= l;
     }
     if (!m_temp_pkt.size && m_pkt.data)
-#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(104<<8)+102)) //ffmpeg-3.0
-        av_free_packet(&m_pkt);
-#else
+#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(24<<8)+102)) //ffmpeg-3.0
         av_packet_unref(&m_pkt);
+#else
+        av_free_packet(&m_pkt);
 #endif
 
     return out_size;
@@ -392,10 +392,10 @@ void DecoderFFmpeg::seek(qint64 pos)
     m_seekTime = timestamp;
     av_seek_frame(ic, -1, timestamp, AVSEEK_FLAG_BACKWARD);
     avcodec_flush_buffers(c);
-#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(104<<8)+102)) //ffmpeg-3.0
-        av_free_packet(&m_pkt);
+#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(24<<8)+102)) //ffmpeg-3.0
+    av_packet_unref(&m_pkt);
 #else
-        av_packet_unref(&m_pkt);
+    av_free_packet(&m_pkt);
 #endif
     m_temp_pkt.size = 0;
 }
@@ -417,10 +417,10 @@ void DecoderFFmpeg::fillBuffer()
             if(m_pkt.stream_index != wma_idx)
             {
                 if(m_pkt.data)
-#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(104<<8)+102)) //ffmpeg-3.0
-                    av_free_packet(&m_pkt);
-#else
+#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(24<<8)+102)) //ffmpeg-3.0
                     av_packet_unref(&m_pkt);
+#else
+                    av_free_packet(&m_pkt);
 #endif
                 m_temp_pkt.size = 0;
                 continue;
@@ -481,10 +481,10 @@ void DecoderFFmpeg::fillBuffer()
             if(c->codec_id == AV_CODEC_ID_SHORTEN || c->codec_id == AV_CODEC_ID_TWINVQ)
             {
                 if(m_pkt.data)
-#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(104<<8)+102)) //ffmpeg-3.0
-                    av_free_packet(&m_pkt);
-#else
+#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(24<<8)+102)) //ffmpeg-3.0
                     av_packet_unref(&m_pkt);
+#else
+                    av_free_packet(&m_pkt);
 #endif
                 m_pkt.data = 0;
                 m_temp_pkt.size = 0;
@@ -495,10 +495,10 @@ void DecoderFFmpeg::fillBuffer()
         else if(m_output_at == 0)
         {
             if(m_pkt.data)
-#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(104<<8)+102)) //ffmpeg-3.0
-                av_free_packet(&m_pkt);
-#else
+#if (LIBAVCODEC_VERSION_INT >= ((57<<16)+(24<<8)+102)) //ffmpeg-3.0
                 av_packet_unref(&m_pkt);
+#else
+                av_free_packet(&m_pkt);
 #endif
             m_pkt.data = 0;
             m_temp_pkt.size = 0;
