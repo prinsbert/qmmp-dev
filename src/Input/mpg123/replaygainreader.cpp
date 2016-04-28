c3913e6baee08d8ab404f20ab1aba7699d477c55
trialuser02 <trialuser02>
2016-04-28 05:50:01 +0000

    using read-only mode for ReplayGain
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp-plugin-pack@6274 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Input/mpg123/replaygainreader.cpp
+++ src/Input/mpg123/replaygainreader.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009-2014 by Ilya Kotov                                 *
+ *   Copyright (C) 2009-2016 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -25,11 +25,20 @@
 #include <taglib/id3v1tag.h>
 #include <taglib/id3v2header.h>
 #include <taglib/textidentificationframe.h>
+#if (TAGLIB_MAJOR_VERSION > 1) || ((TAGLIB_MAJOR_VERSION == 1) && (TAGLIB_MINOR_VERSION >= 8))
+#include <taglib/tfilestream.h>
+#include <taglib/id3v2framefactory.h>
+#endif
 #include "replaygainreader.h"
 
 ReplayGainReader::ReplayGainReader(const QString &path)
 {
-    TagLib::MPEG::File fileRef(path.toLocal8Bit().constData());
+#if (TAGLIB_MAJOR_VERSION > 1) || ((TAGLIB_MAJOR_VERSION == 1) && (TAGLIB_MINOR_VERSION >= 8))
+    TagLib::FileStream stream(QStringToFileName(path), true);
+    TagLib::MPEG::File fileRef(&stream, TagLib::ID3v2::FrameFactory::instance());
+#else
+    TagLib::MPEG::File fileRef(QStringToFileName(path));
+#endif
     if(fileRef.ID3v2Tag())
         readID3v2(fileRef.ID3v2Tag());
     if(m_values.isEmpty() && fileRef.APETag())
