875f34f57fb9ca578acbfecd12127bb8e4d160f6
trialuser02 <trialuser02>
2016-10-26 15:03:04 +0000

    win32 toolchain: updated rusxmms patch
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@6808 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/taglib-rusxmms/taglib-rusxmms.sh
+++ extras/package/win32/contrib/taglib-rusxmms/taglib-rusxmms.sh
@@ -15,7 +15,7 @@ case $1 in
     cd temp
     tar xvzf $NAME-$VERSION.tar.gz
     cd $NAME-$VERSION
-    cat ../../taglib-1.11-ds-rusxmms.patch | patch -p1 --verbose
+    cat ../../taglib-1.11.1-ds-rusxmms.patch | patch -p1 --verbose
     cmake ./ -DCMAKE_INSTALL_PREFIX=${PREFIX}/taglib-rusxmms -G "MSYS Makefiles" -DZLIB_ROOT=${ZLIB_ROOT} \
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF -DCMAKE_CXX_FLAGS="-I${PREFIX}/include" -DCMAKE_SHARED_LINKER_FLAGS="-L${PREFIX}/lib" \
     -DBUILD_SHARED_LIBS=ON
