c1d4a2c5ae77e1223a1b41a9239e2450a7ae3b9d
trialuser02 <trialuser02>
2016-11-05 15:20:55 +0000

    updated Russian translation
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@6877 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Visual/goom/translations/goom_plugin_ru.ts
+++ qmmp-plugin-pack/src/Visual/goom/translations/goom_plugin_ru.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../goomwidget.cpp" line="158"/>
         <source>%1 - %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 - %2</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="209"/>
         <source>Refresh Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Частота обновления</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="212"/>
         <source>60 fps</source>
-        <translation type="unfinished"></translation>
+        <translation>60 гц</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="213"/>
         <source>50 fps</source>
-        <translation type="unfinished"></translation>
+        <translation>50 Гц</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="214"/>
         <source>25 fps</source>
-        <translation type="unfinished"></translation>
+        <translation>25 Гц</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="220"/>
         <source>&amp;Show Title</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Показывать название</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="223"/>
         <source>&amp;Full Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Полноэкранный режим</translation>
     </message>
     <message>
         <location filename="../goomwidget.cpp" line="223"/>
@@ -49,32 +49,32 @@
     <message>
         <location filename="../visualgoomfactory.cpp" line="31"/>
         <source>Goom</source>
-        <translation type="unfinished"></translation>
+        <translation>Goom</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="50"/>
         <source>About Goom Visual Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>О модуле визуализации Goom</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="51"/>
         <source>Qmmp Goom Visual Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль визуализации Goom для Qmmp</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="52"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="53"/>
         <source>Based on the source code from the Goom project</source>
-        <translation type="unfinished"></translation>
+        <translation>Основан на исходном коде проекта Goom</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="54"/>
         <source>Goom project developers:</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчики проекта Goom:</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="55"/>
