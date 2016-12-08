4d8705c67ff8b4749df32699ccb739bcec1b1c7a
trialuser02 <trialuser02>
2016-12-08 17:28:51 +0000

    ffmpeg: enable adts aac support by default
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.2@6908 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/ffmpeg/decoderffmpegfactory.cpp
+++ src/plugins/Input/ffmpeg/decoderffmpegfactory.cpp
@@ -84,7 +84,7 @@ const DecoderProperties DecoderFFmpegFactory::properties() const
 {
     QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
     QStringList filters;
-    filters << "*.wma" << "*.ape" << "*.tta" << "*.m4a" << "*.ra" << "*.shn" << "*.vqf" << "*.ac3";
+    filters << "*.wma" << "*.ape" << "*.tta" << "*.m4a" << "*.aac" << "*.ra" << "*.shn" << "*.vqf" << "*.ac3";
     filters = settings.value("FFMPEG/filters", filters).toStringList();
 
     if(!avcodec_find_decoder(AV_CODEC_ID_WMAV1))
