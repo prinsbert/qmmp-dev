3ef0ca1e8e3fcad84be1647833b53881a9994f51
trialuser02 <trialuser02>
2016-12-19 09:43:01 +0000

    win32 toolchain: removed obsolete patch
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@6916 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/game-music-emu/game-music-emu.sh
+++ extras/package/win32/contrib/game-music-emu/game-music-emu.sh
@@ -13,7 +13,6 @@ case $1 in
     cd temp
     tar xvjf $NAME-$VERSION.tar.bz2
     cd $NAME-$VERSION
-    cat ../../gme_fix_left_shift.patch | patch -p0
     cmake ./ -DCMAKE_INSTALL_PREFIX=$PREFIX -G "MSYS Makefiles" -DCMAKE_COLOR_MAKEFILE:BOOL=OFF
     make -j${JOBS}
     make install
