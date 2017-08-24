4f5dec0fe2d11db07854a66b5ed7620b86cffffd
motsyo <motsyo>
2017-08-24 21:19:55 +0000

    updated Ukrainian translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@7388 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/General/history/translations/history_plugin_ru.ts
+++ qmmp-plugin-pack/src/General/history/translations/history_plugin_ru.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>DateInputDialog</name>
     <message>
@@ -22,7 +22,7 @@
     <message>
         <location filename="../historyfactory.cpp" line="32"/>
         <source>Listening History Plugin</source>
-        <translation>Модулья журнала</translation>
+        <translation>Модуль журнала</translation>
     </message>
     <message>
         <location filename="../historyfactory.cpp" line="52"/>
