feeddb78f08259f241b1429e15bdded11e03115c
trialuser02 <trialuser02>
2017-11-01 16:57:36 +0000

    sync translation
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.2@7687 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Effect/bs2b/translations/bs2b_plugin_uk_UA.ts
+++ src/plugins/Effect/bs2b/translations/bs2b_plugin_uk_UA.ts
@@ -36,12 +36,12 @@
     <message>
         <location filename="../effectbs2bfactory.cpp" line="59"/>
         <source>Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Ілля Котов &lt;forkotov02@ya.ru&gt;</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="60"/>
         <source>Sebastian Pipping &lt;sebastian@pipping.org&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
