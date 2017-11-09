b0769490e4ddd1ef3f3aaee5b4387bf4d02985e0
trialuser02 <trialuser02>
2017-11-09 16:27:46 +0000

    updated Japanese translation (RyoTa SimaMoto)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.2@7692 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/cdaudio/translations/cdaudio_plugin_ja.ts
+++ src/plugins/Input/cdaudio/translations/cdaudio_plugin_ja.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../decodercdaudiofactory.cpp" line="88"/>
         <source>Compiled against libcdio-%1 and libcddb-%2</source>
-        <translation>libcdio-%1 と libcddb-%2 対応でコンパイル</translation>
+        <translation>libcdio-%1 と libcddb-%2 を使用してコンパイル</translation>
     </message>
     <message>
         <location filename="../decodercdaudiofactory.cpp" line="90"/>
