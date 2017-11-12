95e08b4a16fc501792e52ceafa2026eccb1cf103
trialuser02 <trialuser02>
2017-11-12 12:04:27 +0000

    updated Indonesian translation (Wantoyo)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@7713 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Input/mpg123/translations/mpg123_plugin_id.ts
+++ qmmp-plugin-pack/src/Input/mpg123/translations/mpg123_plugin_id.ts
@@ -1,42 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="id">
+<TS version="2.0" language="id_ID">
 <context>
-    <name>DecoderMADFactory</name>
+    <name>DecoderMPG123Factory</name>
     <message>
-        <location filename="../decodermadfactory.cpp" line="119"/>
-        <source>MPEG Plugin</source>
-        <translation>Plugin MPEG</translation>
-    </message>
-    <message>
-        <location filename="../decodermadfactory.cpp" line="122"/>
-        <source>MPEG Files</source>
-        <translation>File MPEG</translation>
-    </message>
-    <message>
-        <location filename="../decodermadfactory.cpp" line="278"/>
-        <source>About MPEG Audio Plugin</source>
-        <translation>Tentang Plugin Audio MPEG</translation>
-    </message>
-    <message>
-        <location filename="../decodermadfactory.cpp" line="279"/>
-        <source>Qmmp MPEG Audio Plugin</source>
-        <translation>Plugin Audio MPEG Qmmp</translation>
-    </message>
-    <message>
-        <location filename="../decodermadfactory.cpp" line="280"/>
-        <source>Compiled against libmad version:</source>
-        <translation>Dikompilasi terhadap versi libmad:</translation>
-    </message>
-    <message>
-        <location filename="../decodermadfactory.cpp" line="284"/>
-        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <location filename="../decodermpg123factory.cpp" line="97"/>
+        <source>MPG123 Plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../decodermadfactory.cpp" line="285"/>
-        <source>Source code based on mq3 and madplay projects</source>
-        <translation>Kode sumber berdasarkan pada mq3 dan proyek madplay</translation>
+        <location filename="../decodermpg123factory.cpp" line="100"/>
+        <source>MPEG Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpg123factory.cpp" line="257"/>
+        <source>About MPG123 Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpg123factory.cpp" line="258"/>
+        <source>Qmmp MPG123 Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpg123factory.cpp" line="259"/>
+        <source>This plugin provides MPEG v1/2 layer1/2/3 support
+with use of libmpg123 library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpg123factory.cpp" line="261"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -44,27 +40,27 @@
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="70"/>
         <source>Format</source>
-        <translation>Format</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="72"/>
         <source>Bitrate</source>
-        <translation>Bitrate</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="72"/>
         <source>kbps</source>
-        <translation>kbps</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="74"/>
         <source>Sample rate</source>
-        <translation>Sample rate</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="74"/>
         <source>Hz</source>
-        <translation>Hz</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="78"/>
@@ -72,49 +68,49 @@
         <location filename="../mpegmetadatamodel.cpp" line="84"/>
         <location filename="../mpegmetadatamodel.cpp" line="87"/>
         <source>Mode</source>
-        <translation>Mode</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="90"/>
         <source>KB</source>
-        <translation>KB</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="91"/>
         <source>File size</source>
-        <translation>Ukuran file</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="93"/>
         <location filename="../mpegmetadatamodel.cpp" line="95"/>
         <source>Protection</source>
-        <translation>Perlindungan</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="93"/>
         <location filename="../mpegmetadatamodel.cpp" line="97"/>
         <location filename="../mpegmetadatamodel.cpp" line="101"/>
         <source>Yes</source>
-        <translation>Ya</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="95"/>
         <location filename="../mpegmetadatamodel.cpp" line="99"/>
         <location filename="../mpegmetadatamodel.cpp" line="103"/>
         <source>No</source>
-        <translation>Tidak</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="97"/>
         <location filename="../mpegmetadatamodel.cpp" line="99"/>
         <source>Copyright</source>
-        <translation>Hak cipta</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mpegmetadatamodel.cpp" line="101"/>
         <location filename="../mpegmetadatamodel.cpp" line="103"/>
         <source>Original</source>
-        <translation>Asli</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -122,70 +118,70 @@
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>MPEG Plugin Settings</source>
-        <translation>Setelan Plugin MPEG</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="29"/>
         <source>Tag Priority</source>
-        <translation>Prioritas Tag</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="41"/>
         <source>First:</source>
-        <translation>Pertama:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="61"/>
         <location filename="../settingsdialog.ui" line="110"/>
         <location filename="../settingsdialog.ui" line="159"/>
         <source>ID3v1</source>
-        <translation>ID3v1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="66"/>
         <location filename="../settingsdialog.ui" line="115"/>
         <location filename="../settingsdialog.ui" line="164"/>
         <source>ID3v2</source>
-        <translation>ID3v2</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="71"/>
         <location filename="../settingsdialog.ui" line="120"/>
         <location filename="../settingsdialog.ui" line="169"/>
         <source>APE</source>
-        <translation>APE</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="76"/>
         <location filename="../settingsdialog.ui" line="125"/>
         <location filename="../settingsdialog.ui" line="174"/>
         <source>Disabled</source>
-        <translation>Dinonaktifkan</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="90"/>
         <source>Second:</source>
-        <translation>Detik:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="139"/>
         <source>Third:</source>
-        <translation>Ketiga:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="198"/>
         <source>Encodings</source>
-        <translation>Pengkodean</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="210"/>
         <source>ID3v1 encoding:</source>
-        <translation>Pengkodean ID3v1:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="236"/>
         <source>ID3v2 encoding:</source>
-        <translation>Pengkodean ID3v2:</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
