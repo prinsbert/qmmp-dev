377ebeade22599ce11cf4618a2039057f16fc9b7
trialuser02 <trialuser02>
2017-11-12 15:45:03 +0000

    updated Chezh translation (Jaroslav Lichtblau)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp-plugin-pack@7716 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Engines/ffvideo/translations/ffvideo_plugin_cs.ts
+++ src/Engines/ffvideo/translations/ffvideo_plugin_cs.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="cs_CZ">
+<TS version="2.0" language="cs">
 <context>
     <name>FFVideoFactory</name>
     <message>
@@ -11,7 +11,7 @@
     <message>
         <location filename="../ffvideofactory.cpp" line="45"/>
         <source>Video Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Videosoubory</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="139"/>
@@ -26,7 +26,7 @@
     <message>
         <location filename="../ffvideofactory.cpp" line="141"/>
         <source>Compiled against:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kompilováno s:</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="151"/>
@@ -39,12 +39,12 @@
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="52"/>
         <source>Length</source>
-        <translation type="unfinished"></translation>
+        <translation>Délka</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="53"/>
         <source>File size</source>
-        <translation type="unfinished"></translation>
+        <translation>Velikost souboru</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="53"/>
@@ -54,7 +54,7 @@
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="54"/>
         <source>Bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Datový tok</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="54"/>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="62"/>
         <source>Audio bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Datový tok audia</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="63"/>
@@ -90,7 +90,7 @@
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="71"/>
         <source>Video bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Datový tok videa</translation>
     </message>
 </context>
 <context>
