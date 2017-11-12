5fd75502ac08ef0ab8c446a32c647ae605fac12f
trialuser02 <trialuser02>
2017-11-12 16:13:13 +0000

    added Finnish translation (Jiri Grönroos)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@7718 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ qmmp-plugin-pack/src/Visual/goom/translations/goom_plugin_fi.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.0">
+<context>
+    <name>GoomWidget</name>
+    <message>
+        <location filename="../goomwidget.cpp" line="137"/>
+        <source>%1 - %2</source>
+        <translation>%1 - %2</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="187"/>
+        <source>Refresh Rate</source>
+        <translation>Päivitystaajuus</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="190"/>
+        <source>60 fps</source>
+        <translation>60 fps</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="191"/>
+        <source>50 fps</source>
+        <translation>50 fps</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="192"/>
+        <source>25 fps</source>
+        <translation>25 fps</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="198"/>
+        <source>&amp;Show Title</source>
+        <translation>&amp;Näytä nimike</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="201"/>
+        <source>&amp;Full Screen</source>
+        <translation>&amp;Koko näyttö</translation>
+    </message>
+    <message>
+        <location filename="../goomwidget.cpp" line="201"/>
+        <source>F</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>VisualGoomFactory</name>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="31"/>
+        <source>Goom</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="50"/>
+        <source>About Goom Visual Plugin</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="51"/>
+        <source>Qmmp Goom Visual Plugin</source>
+        <translation>Qmmp:n Goom-visualisointiliitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Toteuttanut: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="53"/>
+        <source>Based on the source code from the Goom project</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="54"/>
+        <source>Goom project developers:</source>
+        <translation>Goom-projektin kehittäjät:</translation>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="55"/>
+        <source>Jean-Christophe Hoelt &lt;jeko@ios-software.com&gt;</source>
+        <translation>Jean-Christophe Hoelt &lt;jeko@ios-software.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../visualgoomfactory.cpp" line="56"/>
+        <source>Guillaume Borios &lt;gyom@ios-software.com&gt;</source>
+        <translation>Guillaume Borios &lt;gyom@ios-software.com&gt;</translation>
+    </message>
+</context>
+</TS>
