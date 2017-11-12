3fac1f90af1df19041374fa59f4da72c95e3d002
trialuser02 <trialuser02>
2017-11-12 16:13:13 +0000

    added Finnish translation (Jiri Grönroos)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp-plugin-pack@7718 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/Input/ffap/translations/ffap_plugin_fi.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.0">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="48"/>
+        <source>FFap Plugin</source>
+        <translation>FFap-liitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="50"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="158"/>
+        <source>About FFap Audio Plugin</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="159"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation>Qmmp:n FFap-ääniliitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="160"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="161"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Toteuttanut: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="162"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="61"/>
+        <source>Length</source>
+        <translation>Kesto</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="62"/>
+        <source>Sample rate</source>
+        <translation>Näytteenottotaajuus</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="62"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="63"/>
+        <source>Channels</source>
+        <translation>Kanavat</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="64"/>
+        <source>Bitrate</source>
+        <translation>Bittinopeus</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="64"/>
+        <source>kbps</source>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="65"/>
+        <source>File size</source>
+        <translation>Tiedostokoko</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="65"/>
+        <source>KB</source>
+        <translation>kt</translation>
+    </message>
+</context>
+</TS>
