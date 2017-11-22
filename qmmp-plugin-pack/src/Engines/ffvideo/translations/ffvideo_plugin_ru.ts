a23f0fdbec6a04085b7c5001434f027aa077e74d
trialuser02 <trialuser02>
2017-11-22 20:57:47 +0000

    updated Russian translation (Alexey Loginov)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@7729 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Engines/ffvideo/translations/ffvideo_plugin_ru.ts
+++ qmmp-plugin-pack/src/Engines/ffvideo/translations/ffvideo_plugin_ru.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.0" language="ru">
 <context>
     <name>FFVideoFactory</name>
     <message>
         <location filename="../ffvideofactory.cpp" line="40"/>
         <source>FFmpeg Video Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Видео-модуль FFmpeg</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="45"/>
         <source>Video Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Файлы видео</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="139"/>
         <source>About FFVideo Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>О видео-модуле FFVideo</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="140"/>
         <source>FFmpeg-based video plugin for Qmmp</source>
-        <translation type="unfinished"></translation>
+        <translation>Видео-модуль для Qmmp, основанный на FFmpeg</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="141"/>
         <source>Compiled against:</source>
-        <translation type="unfinished"></translation>
+        <translation>Собрано с библиотеками:</translation>
     </message>
     <message>
         <location filename="../ffvideofactory.cpp" line="151"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@ya.ru&gt;</translation>
     </message>
 </context>
 <context>
@@ -39,58 +39,58 @@
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="52"/>
         <source>Length</source>
-        <translation type="unfinished"></translation>
+        <translation>Длительность</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="53"/>
         <source>File size</source>
-        <translation type="unfinished"></translation>
+        <translation>Размер файла</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="53"/>
         <source>%1 KB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 КБ</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="54"/>
         <source>Bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Битовая частота</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="54"/>
         <location filename="../ffvideometadatamodel.cpp" line="62"/>
         <source>%1 kbps</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Кб/с</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="62"/>
         <source>Audio bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Битовая частота аудио</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="63"/>
         <source>Audio sample rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Частота дискретизации аудио:</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="63"/>
         <source>%1 Hz</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Гц</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="64"/>
         <source>Audio channels</source>
-        <translation type="unfinished"></translation>
+        <translation>Аудио каналы</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="70"/>
         <source>Video size</source>
-        <translation type="unfinished"></translation>
+        <translation>Размер видео</translation>
     </message>
     <message>
         <location filename="../ffvideometadatamodel.cpp" line="71"/>
         <source>Video bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Битовая частота видео</translation>
     </message>
 </context>
 <context>
@@ -98,12 +98,12 @@
     <message>
         <location filename="../videowindow.cpp" line="37"/>
         <source>FFmpeg Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Видео FFmpeg</translation>
     </message>
     <message>
         <location filename="../videowindow.cpp" line="51"/>
         <source>Alt+Return</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+Enter</translation>
     </message>
 </context>
 </TS>
