fe55f0018e183f0e222cfdbf57128d9848510bea
trialuser02 <trialuser02>
2018-03-20 20:20:39 +0000

    updated changelogs; updated Ukrainian translation (Gennadi Motsyo)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.11@7894 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Effect/soxr/translations/soxr_plugin_uk_UA.ts
+++ src/plugins/Effect/soxr/translations/soxr_plugin_uk_UA.ts
@@ -29,7 +29,7 @@
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>SoX Resampler Settings</source>
-        <translation>Налаштування модуля перетворювача частоти</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="29"/>
