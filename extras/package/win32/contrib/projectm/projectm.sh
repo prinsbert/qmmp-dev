6046f52a7800f3f820c10adfb50bd1a8886acb13
trialuser02 <trialuser02>
2018-07-01 15:28:20 +0000

    win32 toolchain: fixed gcc6 build
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@8087 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/projectm/projectm.sh
+++ extras/package/win32/contrib/projectm/projectm.sh
@@ -16,6 +16,7 @@ case $1 in
     cd projectM-complete-$VERSION-Source
     cat ../../projectm-win32.patch | patch -p1
     cat ../../fix-fbo-crash.patch | patch -p1
+    cat ../../01_fix-gcc6-build.patch | patch -p1
     cd src    
     cmake ./ -DCMAKE_INSTALL_PREFIX=${PREFIX} -G "MSYS Makefiles" \
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF \
