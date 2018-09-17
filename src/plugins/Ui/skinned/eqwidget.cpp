a5dc5f041d38d57fd2045f0ba398cbc948fa2118
trialuser02 <trialuser02>
2018-09-17 17:51:23 +0000

    fixed issue with multiscreen setup (#971)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.11@8312 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/skinned/eqwidget.cpp
+++ src/plugins/Ui/skinned/eqwidget.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006-2017 by Ilya Kotov                                 *
+ *   Copyright (C) 2006-2018 by Ilya Kotov                                 *
  *   forkotov02@ya.ru                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -146,13 +146,10 @@ void EqWidget::readSettings()
     QSettings settings (Qmmp::configFile(), QSettings::IniFormat);
     QDesktopWidget *desktop = qApp->desktop();
     QPoint pos = settings.value("Skinned/eq_pos", QPoint(100, 216)).toPoint();
-    if(!desktop->availableGeometry().contains(pos))
-    {
-        QRect availableGeometry = desktop->availableGeometry();
-        int r = m_skin->ratio();
-        pos.setX(qBound(availableGeometry.left(), pos.x(), availableGeometry.right() - r*275));
-        pos.setY(qBound(availableGeometry.top(), pos.y(), availableGeometry.bottom() - r*116));
-    }
+    int r = m_skin->ratio();
+    QRect availableGeometry = desktop->availableGeometry(desktop->screenNumber(pos));
+    pos.setX(qBound(availableGeometry.left(), pos.x(), availableGeometry.right() - r*275));
+    pos.setY(qBound(availableGeometry.top(), pos.y(), availableGeometry.bottom() - r*116));
     move (pos); //geometry
     readEq();
     m_autoButton->setChecked(settings.value("Skinned/eq_auto", false).toBool());
