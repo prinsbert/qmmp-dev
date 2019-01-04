c3d2faf06ef7978d2f373cabf48370229e89281f
trialuser02 <trialuser02>
2019-01-04 17:04:17 +0000

    sync with trunk
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.12@8584 90c681e8-e032-0410-971d-27865f9a5e38

--- src/app/qmmpstarter.h
+++ src/app/qmmpstarter.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006-2017 by Ilya Kotov                                 *
+ *   Copyright (C) 2006-2019 by Ilya Kotov                                 *
  *   forkotov02@ya.ru                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -71,6 +71,7 @@ private:
     void printVersion();
     void printUserInterfaces();
     void startPlayer();
+    void createInitialConfig();
 
 private:
     MediaPlayer *m_player;
