9fb7991f15769f3e6d9f88362c5dede9a654b4b0
trialuser02 <trialuser02>
2019-01-06 21:06:04 +0000

    removed unimplemented functions
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.2@8598 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/vorbis/decodervorbisfactory.h
+++ src/plugins/Input/vorbis/decodervorbisfactory.h
@@ -47,7 +47,6 @@ public:
     Decoder *create(const QString &path, QIODevice *input);
     MetaDataModel* createMetaDataModel(const QString &path, QObject *parent = 0);
     QList<FileInfo *> createPlayList(const QString &fileName, bool useMetaData, QStringList *);
-    QObject* showDetails(QWidget *parent, const QString &path);
     void showSettings(QWidget *parent);
     void showAbout(QWidget *parent);
     QTranslator *createTranslator(QObject *parent);
