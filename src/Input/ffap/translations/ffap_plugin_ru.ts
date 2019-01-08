36a748579f01ef2e9780fd43a23923799cff908f
trialuser02 <trialuser02>
2019-01-08 21:19:02 +0000

    updated Russian translation (Alexey Loginov)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-plugin-pack-0.12@8628 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Input/ffap/translations/ffap_plugin_ru.ts
+++ src/Input/ffap/translations/ffap_plugin_ru.ts
@@ -44,12 +44,12 @@
     <message>
         <location filename="../ffapmetadatamodel.cpp" line="80"/>
         <source>Samples</source>
-        <translation type="unfinished"></translation>
+        <translation>Сэмплов</translation>
     </message>
     <message>
         <location filename="../ffapmetadatamodel.cpp" line="82"/>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия</translation>
     </message>
 </context>
 </TS>
