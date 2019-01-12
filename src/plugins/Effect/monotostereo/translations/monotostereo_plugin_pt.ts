cf2a6a6fe7e6ec2d768868871b358efdea94441c
trialuser02 <trialuser02>
2019-01-12 20:21:07 +0000

    updated Portuguese translation (Sérgio Marques)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.12@8633 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Effect/monotostereo/translations/monotostereo_plugin_pt.ts
+++ src/plugins/Effect/monotostereo/translations/monotostereo_plugin_pt.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="pt_BR">
+<TS version="2.0" language="pt">
 <context>
     <name>EffectMonoToStereoFactory</name>
     <message>
         <location filename="../effectmonotostereofactory.cpp" line="30"/>
         <source>Mono to Stereo Converter Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Suplemento Mono to Stereo Converter</translation>
     </message>
     <message>
         <location filename="../effectmonotostereofactory.cpp" line="49"/>
         <source>About Mono to Stereo Converter Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Sobre o suplemento Mono to Stereo Converter</translation>
     </message>
     <message>
         <location filename="../effectmonotostereofactory.cpp" line="50"/>
         <source>Qmmp Mono to Stereo Converter Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Suplemento Qmmp Mono to Stereo Converter</translation>
     </message>
     <message>
         <location filename="../effectmonotostereofactory.cpp" line="51"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Desenvolvido por: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
     </message>
 </context>
 </TS>
