c49668f430fb73168d29152152c0ce4b1c2ba440
trialuser02 <trialuser02>
2019-03-16 15:22:39 +0000

    win32 toolchain: fixed libsndfile vulnerability issues
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@8762 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/libsndfile/libsndfile.sh
+++ extras/package/win32/contrib/libsndfile/libsndfile.sh
@@ -13,6 +13,18 @@ case $1 in
     cd temp
     tar xvzf $NAME-$VERSION.tar.gz
     cd $NAME-$VERSION
+    cat ../../CVE-2017-8365.patch | patch -p1
+    cat ../../CVE-2017-8363.patch | patch -p1
+    cat ../../CVE-2017-8362.patch | patch -p1
+    cat ../../CVE-2017-6892.patch | patch -p1
+    cat ../../CVE-2019-3832.patch | patch -p1
+    cat ../../binheader-heapoverflow.patch | patch -p1
+    cat ../../fix_rf64_arm.patch | patch -p1
+    cat ../../fix_typos.patch | patch -p1
+    cat ../../a-ulaw-fix-multiple-buffer-overflows-432.patch | patch -p1
+    cat ../../double64_init-Check-psf-sf.channels-against-upper-bo.patch | patch -p1
+    cat ../../src-wav.c-Fix-heap-read-overflow.patch | patch -p1
+    cat ../../Check-MAX_CHANNELS-in-sndfile-deinterleave.patch | patch -p1
     ./configure --prefix=$PREFIX --enable-shared --disable-static --disable-external-libs --disable-sqlite
     make -j${JOBS}
     make install
