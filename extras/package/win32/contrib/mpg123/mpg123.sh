a68fd1bff53edc54c0ae27465ec2a34633cb78e9
trialuser02 <trialuser02>
2019-05-01 17:17:44 +0000

    win32 toolchain: added mpg123
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@8825 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ extras/package/win32/contrib/mpg123/mpg123.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+NAME=mpg123
+VERSION=1.25.10
+
+case $1 in
+  --download)
+    mkdir -p temp
+    cd temp
+    wget -nc https://www.mpg123.de/download/$NAME-$VERSION.tar.bz2
+  ;;
+  --install)
+    cd temp
+    tar xvjf $NAME-$VERSION.tar.bz2
+    cd $NAME-$VERSION
+    
+    autoreconf -i
+    ./configure --prefix=$PREFIX --disable-debugging --enable-shared --disable-static
+    make -j${JOBS}
+    make install
+
+  ;;
+  --clean)
+    cd temp
+    rm -rf $NAME-$VERSION
+    cd ..
+  ;;
+esac
