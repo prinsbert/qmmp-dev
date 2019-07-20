173dcb1cecb71218dc8de19f36d555e6f8d15e38
motsyo <motsyo>
2019-07-20 17:30:28 +0000

    update Ukrainian translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9005 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Transports/ytb/translations/ytb_plugin_uk_UA.ts
+++ qmmp-plugin-pack/src/Transports/ytb/translations/ytb_plugin_uk_UA.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk_UA">
+<TS version="2.1" language="uk">
 <context>
     <name>YtbInputFactory</name>
     <message>
         <location filename="../ytbinputfactory.cpp" line="32"/>
         <source>Youtube Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль Youtube</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="56"/>
         <source>not found</source>
-        <translation type="unfinished"></translation>
+        <translation>не знайдено</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="58"/>
         <source>About Youtube Transport Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль передачі з Youtube</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="59"/>
         <source>Qmmp Youtube Transport Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль передачі з Youtube для Qmmp</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="60"/>
         <source>This plugin adds feature to play audio from Youtube using &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt; utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль додає функцію відтворення аудіо з YouTube за допомогою утиліти &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="62"/>
         <source>youtube-dl version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>версія youtube-dl: %1</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="63"/>
         <source>Written by: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &amp;lt;forkotov02@ya.ru&amp;gt;</translation>
     </message>
 </context>
 <context>
@@ -44,17 +44,17 @@
     <message>
         <location filename="../ytbinputsource.cpp" line="119"/>
         <source>Uploader</source>
-        <translation type="unfinished"></translation>
+        <translation>Завантажувач</translation>
     </message>
     <message>
         <location filename="../ytbinputsource.cpp" line="120"/>
         <source>Upload date</source>
-        <translation type="unfinished"></translation>
+        <translation>Дата завантаження</translation>
     </message>
     <message>
         <location filename="../ytbinputsource.cpp" line="121"/>
         <source>Duration</source>
-        <translation type="unfinished"></translation>
+        <translation>Тривалість</translation>
     </message>
 </context>
 </TS>
