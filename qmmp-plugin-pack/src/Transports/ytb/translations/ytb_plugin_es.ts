aed0f86be411f6eb7109e32ed4d9ad81bee1afc1
trialuser02 <trialuser02>
2020-03-29 19:44:44 +0000

    updated Spanish translation (Joel Barrios)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9329 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Transports/ytb/translations/ytb_plugin_es.ts
+++ qmmp-plugin-pack/src/Transports/ytb/translations/ytb_plugin_es.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_ES">
+<TS version="2.1" language="es">
 <context>
     <name>YtbInputFactory</name>
     <message>
         <location filename="../ytbinputfactory.cpp" line="36"/>
         <source>Youtube Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Módulo Youtube</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="60"/>
         <source>not found</source>
-        <translation type="unfinished"></translation>
+        <translation>no encontrado</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="62"/>
         <source>About Youtube Transport Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca del Módulo de Transporte Youtube</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="63"/>
         <source>Qmmp Youtube Transport Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Módulo de Transporte Youtube de Qmmp</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="64"/>
         <source>This plugin adds feature to play audio from Youtube using &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt; utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Este módulo añade funciones para reproducir audio desde Youtube utilizando la utilidad &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="66"/>
         <source>youtube-dl version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Versión de youtube-dl: %1</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="67"/>
         <source>Written by: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Escrito por: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</translation>
     </message>
 </context>
 <context>
@@ -44,17 +44,17 @@
     <message>
         <location filename="../ytbinputsource.cpp" line="130"/>
         <source>Uploader</source>
-        <translation type="unfinished"></translation>
+        <translation>Subido por</translation>
     </message>
     <message>
         <location filename="../ytbinputsource.cpp" line="131"/>
         <source>Upload date</source>
-        <translation type="unfinished"></translation>
+        <translation>Fecha de subida</translation>
     </message>
     <message>
         <location filename="../ytbinputsource.cpp" line="132"/>
         <source>Duration</source>
-        <translation type="unfinished"></translation>
+        <translation>Duración</translation>
     </message>
 </context>
 </TS>
