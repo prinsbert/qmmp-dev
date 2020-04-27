e359df6a76595749371967e84188458dead2a67e
trialuser02 <trialuser02>
2020-04-27 16:37:50 +0000

    added youtube-dl ubuntu packages
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9364 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ extras/package/ubuntu/youtube-dl/youtube-dl_deb.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+VERSION=2020.03.24
+UBUNTU_CODENAMES='xenial bionic eoan focal'
+BUILD_ROOT=build-root
+
+
+prepare ()
+{
+    cp ../youtube-dl-$VERSION.tar.gz ./
+    mv ./youtube-dl-$VERSION.tar.gz ./youtube-dl_$VERSION.orig.tar.gz
+}
+
+build ()
+{
+    echo '****'$1'****'
+    mkdir $1
+    cd $1
+    tar xvzf ../../youtube-dl-$VERSION.tar.gz
+    mkdir youtube-dl/debian
+    cp -rv ../../debian-$1/* -t youtube-dl/debian/
+    cp ../youtube-dl_$VERSION.orig.tar.gz ./
+    cd youtube-dl
+    if [ "$1" = "xenial" ] ; then
+        debuild -S -sa -d -kF594F6B4
+    else
+        debuild -S -sd -d -kF594F6B4
+    fi
+    cd ..
+    cd ..
+}
+
+update ()
+{
+    dch -m --newversion ${VERSION}-1ubuntu1~${1}0 -D ${1} -c debian-$1/changelog "New upstream release."
+}
+
+upload ()
+{
+	dput ppa:forkotov02/ppa $1/*.changes
+}
+
+clean ()
+{
+    rm -rf $BUILD_ROOT
+    rm youtube-dl-$VERSION.tar.gz
+}
+
+case $1 in
+    --download)
+		wget https://youtube-dl.org/downloads/latest/youtube-dl-${VERSION}.tar.gz
+    ;;
+    --update)
+		for CODENAME in $UBUNTU_CODENAMES
+		do
+			update $CODENAME
+		done
+    ;;
+    --build)
+		rm -rf $BUILD_ROOT
+		mkdir $BUILD_ROOT
+		cd $BUILD_ROOT
+		prepare
+		for CODENAME in $UBUNTU_CODENAMES
+		do
+			build $CODENAME
+		done
+    ;;
+    --upload)
+       cd $BUILD_ROOT
+       for CODENAME in $UBUNTU_CODENAMES
+		do
+			upload $CODENAME
+		done
+    ;;
+    --clean)
+		clean
+    ;;
+    *)
+		echo "Commands:"
+		echo "--download"
+		echo "--update"
+		echo "--build"
+		echo "--upload"
+		echo "--clean"
+esac
