82c4902dedf25c6642cd32f7f74c8a2d9bce8be1
trialuser02 <trialuser02>
2020-08-15 18:13:17 +0000

    win32 toolchain: fixed mingw-libs.sh
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9481 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/mingw-libs.sh
+++ extras/package/win32/contrib/mingw-libs.sh
@@ -3,7 +3,7 @@
 LIB_NAMES+='pkg-config yasm taglib libmad mpg123 libogg libvorbis flac wavpack libsndfile opus opusfile '
 LIB_NAMES+='libbs2b curl libcdio libgnurx libcddb ffmpeg libmodplug game-music-emu musepack '
 LIB_NAMES+='glew projectm xa libsidplayfp enca soxr '
-LIB_NAMES+='libxml2 librcd librcc taglib-rusxmms' #rusxmms
+LIB_NAMES+='libxml2 librcd librcc taglib-rusxmms ' #rusxmms
 LIB_NAMES+='libbinio adplug' #adplug 
 
 export DEV_PATH=/c/devel
