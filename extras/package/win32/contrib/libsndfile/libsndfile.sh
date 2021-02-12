e6bd33ec9e9886886efc2fa0bfd80276ffdd9782
trialuser02 <trialuser02>
2021-02-12 19:30:35 +0000

    win32 toolchain: libsndfile-1.0.31
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9731 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/libsndfile/libsndfile.sh
+++ extras/package/win32/contrib/libsndfile/libsndfile.sh
@@ -1,30 +1,18 @@
 #!/bin/sh
 
 NAME=libsndfile
-VERSION=1.0.28
+VERSION=1.0.31
 
 case $1 in
   --download)
     mkdir -p temp
     cd temp
-    wget -nc http://www.mega-nerd.com/libsndfile/files/$NAME-$VERSION.tar.gz
+    wget -nc https://github.com/libsndfile/libsndfile/releases/download/$VERSION/$NAME-$VERSION.tar.bz2
   ;;
   --install)
     cd temp
-    tar xvzf $NAME-$VERSION.tar.gz
+    tar xvjf $NAME-$VERSION.tar.bz2
     cd $NAME-$VERSION
-    cat ../../CVE-2017-8365.patch | patch -p1
-    cat ../../CVE-2017-8363.patch | patch -p1
-    cat ../../CVE-2017-8362.patch | patch -p1
-    cat ../../CVE-2017-6892.patch | patch -p1
-    cat ../../CVE-2019-3832.patch | patch -p1
-    cat ../../binheader-heapoverflow.patch | patch -p1
-    cat ../../fix_rf64_arm.patch | patch -p1
-    cat ../../fix_typos.patch | patch -p1
-    cat ../../a-ulaw-fix-multiple-buffer-overflows-432.patch | patch -p1
-    cat ../../double64_init-Check-psf-sf.channels-against-upper-bo.patch | patch -p1
-    cat ../../src-wav.c-Fix-heap-read-overflow.patch | patch -p1
-    cat ../../Check-MAX_CHANNELS-in-sndfile-deinterleave.patch | patch -p1
     ./configure --prefix=$PREFIX --enable-shared --disable-static --disable-external-libs --disable-sqlite
     make -j${JOBS}
     make install
