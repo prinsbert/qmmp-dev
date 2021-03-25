e8b13e80ed6f48039e743ae041ff7fb167f4aa90
trialuser02 <trialuser02>
2021-03-25 15:10:39 +0000

    fixed build
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9783 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/libsidplayfp/libsidplayfp.sh
+++ extras/package/win32/contrib/libsidplayfp/libsidplayfp.sh
@@ -7,7 +7,7 @@ case $1 in
   --download)
     mkdir -p temp
     cd temp
-    wget -nc --no-check-certificate http://sourceforge.net/projects/sidplay-residfp/files/$NAME/2.0/$NAME-$VERSION.tar.gz
+    wget -nc --no-check-certificate http://sourceforge.net/projects/sidplay-residfp/files/$NAME/2.1/$NAME-$VERSION.tar.gz
   ;;
   --install)
     cd temp
