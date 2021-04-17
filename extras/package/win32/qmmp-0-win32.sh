07e189f0d6b50459a73e12d7834cd9faf694f023
trialuser02 <trialuser02>
2021-04-17 19:53:13 +0000

    updated win32 build scripts
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@9819 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/qmmp-0-win32.sh
+++ extras/package/win32/qmmp-0-win32.sh
@@ -115,7 +115,7 @@ create_distr ()
     cp -v ${MINGW32_PATH}/bin/${LIB_NAME} ./
   done
   #third party libs   
-  for LIB_NAME in avcodec-57.dll avformat-57.dll avutil-55.dll glew32.dll libFLAC-8.dll libcddb-2.dll libcdio-13.dll libcdio_cdda-1.dll
+  for LIB_NAME in avcodec-57.dll avformat-57.dll avutil-55.dll glew32.dll libFLAC-8.dll libcddb-2.dll libcdio-19.dll libcdio_cdda-2.dll libcdio_paranoia-2.dll
   do
     cp -v ${PREFIX}/bin/${LIB_NAME} ./
   done
