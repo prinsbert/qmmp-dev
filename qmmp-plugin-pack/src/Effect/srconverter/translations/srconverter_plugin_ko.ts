cb634f986400aec241fce60783bb31469af3d3c8
trialuser02 <trialuser02>
2021-06-06 08:22:05 +0000

    added Korean translation (Jung Hee Lee)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@10056 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ qmmp-plugin-pack/src/Effect/srconverter/translations/srconverter_plugin_ko.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>EffectSRConverterFactory</name>
+    <message>
+        <location filename="../effectsrconverterfactory.cpp" line="30"/>
+        <source>SRC Plugin</source>
+        <translation>SRC 플러그인</translation>
+    </message>
+    <message>
+        <location filename="../effectsrconverterfactory.cpp" line="51"/>
+        <source>About Sample Rate Converter Plugin</source>
+        <translation>샘플 레이트 변환기 플러그인 정보</translation>
+    </message>
+    <message>
+        <location filename="../effectsrconverterfactory.cpp" line="52"/>
+        <source>Qmmp Sample Rate Converter Plugin</source>
+        <translation>Qmmp 샘플 레이트 변환기 플러그인</translation>
+    </message>
+    <message>
+        <location filename="../effectsrconverterfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>작성자: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="13"/>
+        <source>Sample Rate Converter Plugin Settings</source>
+        <translation>Sample Rate Converter 플러그인 설정</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="19"/>
+        <source>Sample Rate (Hz):</source>
+        <translation>샘플 레이트 (Hz):</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="39"/>
+        <source>Interpolation Engine:</source>
+        <translation>보간 엔진:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="47"/>
+        <source>Best Sinc Interpolation</source>
+        <translation>최고의 Sinc 보간</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="52"/>
+        <source>Medium Sinc Interpolation</source>
+        <translation>중간 Sinc 보간</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="57"/>
+        <source>Fastest Sinc Interpolation</source>
+        <translation>가장 빠른 Sinc 보간</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="62"/>
+        <source>ZOH Interpolation</source>
+        <translation>ZOH 보간</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="67"/>
+        <source>Linear Interpolation</source>
+        <translation>선형 보간</translation>
+    </message>
+</context>
+</TS>
---
+++ qmmp-plugin-pack/src/Input/ffap/translations/ffap_plugin_ko.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="43"/>
+        <source>FFap Plugin</source>
+        <translation>FFap 플러그인</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="45"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation>Monkey&apos;s 오디오 파일</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="150"/>
+        <source>About FFap Audio Plugin</source>
+        <translation>FFap 오디오 플러그인 정보</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="151"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation>Qmmp FFap 오디오 플러그인</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="152"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation>이 플러그인은 Monkey&apos;s Audio(APE) 지원을 제공합니다</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="153"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>작성자: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="154"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation>deadbeef, FFmpeg 및 rockbox의 코드에 기반함</translation>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="65"/>
+        <source>Samples</source>
+        <translation>샘플</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="66"/>
+        <source>Version</source>
+        <translation>버전</translation>
+    </message>
+</context>
+</TS>
