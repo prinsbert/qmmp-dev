5e3785aa6365daf92da54dee7da09d706492a6e8
trialuser02 <trialuser02>
2021-06-29 15:50:27 +0000

    win32 toolchain: added qt6 support
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@10131 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ extras/package/win32/qmmp-2-win32.sh
@@ -0,0 +1,200 @@
+#!/bin/sh
+
+QMMP_VERSION=2.0.0
+QMMP_PLUGIN_PACK_VERSION=2.0.0
+
+export DEV_PATH=/c/devel
+export MINGW32_PATH=${DEV_PATH}/mingw32
+export QT5_PATH=${DEV_PATH}/qt6
+export ZLIB_ROOT=${MINGW32_PATH}/i686-w64-mingw32
+export PREFIX=${DEV_PATH}/mingw32-libs
+export SVN_PATH=/c/Program\ Files/Subversion/bin
+export PATH=${PATH}:${MINGW32_PATH}/bin:${QT6_PATH}/bin:${PREFIX}/bin:${SVN_PATH}
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig 
+
+export JOBS=2
+
+
+
+download_qmmp_tarball()
+{
+  mkdir -p tmp
+  cd tmp
+  echo 'downloading qmmp...'
+  wget -nc http://qmmp.ylsoftware.com/files/qmmp-${QMMP_VERSION}.tar.bz2
+  tar xvjf qmmp-${QMMP_VERSION}.tar.bz2
+  cd ..
+}
+
+download_plugins_tarball()
+{
+  mkdir -p tmp
+  cd tmp
+  echo 'downloading qmmp-plugin-pack...'
+  wget -nc http://qmmp.ylsoftware.com/files/plugins/qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}.tar.bz2
+  tar xvjf qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}.tar.bz2
+  cd ..
+}
+
+download_qmmp_adplug_archive()
+{
+  mkdir -p tmp
+  cd tmp
+  echo 'downloading qmmp-adplug...'
+  wget -nc https://github.com/cspiegel/qmmp-adplug/archive/master.zip
+  7za x -y master.zip
+  cd ..
+}
+
+download_qmmp_svn()
+{
+  mkdir -p tmp
+  cd tmp
+  echo 'downloading qmmp...'
+  #svn checkout svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp qmmp-${QMMP_VERSION}
+  svn checkout svn://svn.code.sf.net/p/qmmp-dev/code/branches/qmmp-2.0 qmmp-${QMMP_VERSION}
+  cd ..
+}
+
+download_plugins_svn()
+{
+  mkdir -p tmp
+  cd tmp
+  echo 'downloading qmmp-plugin-pack...'
+  #svn checkout svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp-plugin-pack qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}
+  svn checkout svn://svn.code.sf.net/p/qmmp-dev/code/branches/qmmp-plugin-pack-2.0 qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}
+  cd ..
+}
+
+build ()
+{ 
+  cd qmmp-${QMMP_VERSION}
+  qmake CONFIG+=release
+  mingw32-make -j${JOBS}
+  cd ..
+  cd qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}
+  qmake CONFIG+=release INCLUDEPATH+=`dirs`/../qmmp-${QMMP_VERSION}/src QMAKE_LIBDIR+=`dirs`/../qmmp-${QMMP_VERSION}/bin
+  mingw32-make -j${JOBS}
+  cd ..
+  cd qmmp-adplug-master
+  qmake CONFIG+=release INCLUDEPATH+=`dirs`/../qmmp-${QMMP_VERSION}/src QMAKE_LIBDIR+=`dirs`/../qmmp-${QMMP_VERSION}/bin LIBS+=-lqmmp1 QT+=widgets CONFIG+=link_pkgconfig PKGCONFIG+=adplug \
+  CONFIG+=hide_symbols
+  mingw32-make -j${JOBS}
+  cd ..
+}
+
+create_distr ()
+{
+  mkdir -p qmmp-distr
+  cd qmmp-distr
+  mkdir -p translations
+  cp -v ../../*.txt ./
+  cp -v ../../qmmp-2.x/*.txt ./
+  cp -v ../../qmmp-2.x/*.nsi ./
+  cp -v ../../qmmp-2.x/*.conf ./
+  cp -v ../../qmmp-2.x/*.default ./
+  cp -v ../../unzip.exe ./
+  cp -rv ../../themes ./
+  cp -rv ../../skins ./
+  cp -v ../qmmp-${QMMP_VERSION}/bin/*.exe ./
+  cp -v ../qmmp-${QMMP_VERSION}/bin/*.dll ./
+  cp -rv ../qmmp-${QMMP_VERSION}/bin/plugins ./
+  cp -rv ../qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}/bin/plugins ./
+  find . -type f -name *.a -delete
+  find . -type d -name ".svn" | xargs rm -rf
+  cp -v ../qmmp-${QMMP_VERSION}/ChangeLog ./ChangeLog.txt
+  cp -v ../qmmp-${QMMP_VERSION}/ChangeLog.rus ./ChangeLog.rus.txt
+  u2d ./ChangeLog.txt
+  u2d ./ChangeLog.rus.txt
+  #versions
+  ../../contrib/mingw-libs.sh --print-versions -v '' > versions.txt
+
+  #Qt libs
+  for LIB_NAME in Qt6Core.dll Qt6Gui.dll Qt6Widgets.dll Qt6Network.dll Qt6Sql.dll Qt6OpenGLWidgets.dll
+  do
+    cp -v ${QT6_PATH}/bin/${LIB_NAME} ./
+  done
+  #Qt plugins
+  mkdir -p plugins/imageformats plugins/platforms plugins/sqldrivers plugins/styles
+  cp -v ${QT6_PATH}/plugins/imageformats/*.dll ./plugins/imageformats  
+  for LIB_NAME in qwindows.dll
+  do
+    cp -v ${QT6_PATH}/plugins/platforms/${LIB_NAME} ./plugins/platforms
+  done
+  for LIB_NAME in qsqlite.dll
+  do
+    cp -v ${QT6_PATH}/plugins/sqldrivers/${LIB_NAME} ./plugins/sqldrivers
+  done
+  for LIB_NAME in qwindowsvistastyle.dll
+  do
+    cp -v ${QT6_PATH}/plugins/styles/${LIB_NAME} ./plugins/styles
+  done
+  #translations
+  cp -v ${QT6_PATH}/translations/qtbase_??.qm ./translations
+  #mingw32 libs
+  for LIB_NAME in libgcc_s_dw2-1.dll libstdc++-6.dll libwinpthread-1.dll libgomp-1.dll
+  do
+    cp -v ${MINGW32_PATH}/bin/${LIB_NAME} ./
+  done
+  #third party libs   
+  for LIB_NAME in avcodec-57.dll avformat-57.dll avutil-55.dll glew32.dll libFLAC-8.dll libcddb-2.dll libcdio-19.dll libcdio_cdda-2.dll libcdio_paranoia-2.dll
+  do
+    cp -v ${PREFIX}/bin/${LIB_NAME} ./
+  done
+  for LIB_NAME in libcurl.dll libenca-0.dll libgme.dll libgnurx-0.dll libmad-0.dll libmodplug-1.dll libmpcdec.dll libogg-0.dll
+  do
+    cp -v ${PREFIX}/bin/${LIB_NAME} ./
+  done
+  for LIB_NAME in libopus-0.dll libopusfile-0.dll libprojectM.dll libsidplayfp-*.dll libsndfile-1.dll libtag.dll libvorbis-0.dll
+  do
+    cp -v ${PREFIX}/bin/${LIB_NAME} ./
+  done
+  for LIB_NAME in libvorbisfile-3.dll libwavpack-1.dll libsoxr.dll libmpg123-0.dll librcd-0.dll
+  do
+    cp -v ${PREFIX}/bin/${LIB_NAME} ./
+  done
+  #projectM presets
+  cp -rv ${PREFIX}/share/projectM/ ./	     
+  #adplug
+  mkdir -p adplug
+  for LIB_NAME in libadplug*.dll libbinio*.dll
+  do
+    cp -v ${PREFIX}/bin/${LIB_NAME} ./adplug/
+  done
+  cp -v ../qmmp-adplug-master/release/*.dll ./adplug/
+  cd ..
+}
+
+
+case $1 in
+  --download)
+    download_qmmp_tarball
+    download_plugins_tarball
+    #download_qmmp_svn
+    #download_plugins_svn
+    download_qmmp_adplug_archive
+  ;;
+  --install)
+    cd tmp
+    build
+    create_distr
+    find qmmp-distr -type f -name *.dll   | xargs strip -v
+    find qmmp-distr -type f -name *.exe   | xargs strip -v 
+  ;;
+  --clean)
+    cd tmp
+    rm -rf qmmp-distr
+    rm -rf qmmp-${QMMP_VERSION}
+    rm -rf qmmp-plugin-pack-${QMMP_PLUGIN_PACK_VERSION}
+  ;;
+  --clean-src)
+    rm -rf tmp
+  ;;
+  *)
+    echo "Commands:"
+    echo "--download"
+    echo "--install"
+    echo "--clean"
+    echo "--clean-src"
+  ;;
+esac
---
+++ extras/package/win32/qmmp-2.x/README-win32.txt
@@ -0,0 +1,71 @@
+Qt-based Multimedia Player for Windows
+
+License: GNU General Public License v2.0 or later
+Offical web page: http://qmmp.ylsoftware.com
+Bug tracker: https://sourceforge.net/p/qmmp-dev/tickets/
+Translation: https://www.transifex.com/qmmp-development-team/
+Maintainer: Ilya Kotov <forkotov02@ya.ru> or <iokotov@astralinux.ru>
+
+Known issues:
+  - disabled plugins: mms, wildmidi, aac, jack, pulseaudio, oss4, alsa, mplayer,
+    mpris, converter, kdenotify, gnome hotkey, archive, icecast;
+  - packed Xmms skins (i.e. *.tar.gz, *.tar.bz2) are not supported. You should unpack them before usage.
+
+Extra plugins (see http://qmmp.ylsoftware.com/plugins.php):
+  - FFap - enhanced Monkey's Audio (APE) decoder;
+  - Goom - audio visualization based on goom project.
+
+Third-party plugins:
+  - qmmp-adplug - AdPlug-based AdLib input plugin for Qmmp (Web page: https://github.com/cspiegel/qmmp-adplug)
+
+This archive also contains third-party libraries. Source code of these libraries
+is freely available on the Internet.
+
+List of the third-party libraries.
+
+  Name                Version    Web page
+
+  MinGW-w64           4.3.5      http://mingw-w64.sourceforge.net/
+  Qt                  6.1.1      http://qt.io/
+  unzip               5.51       http://www.info-zip.org/pub/infozip/UnZip.html
+  taglib              1.12       http://taglib.github.io/
+  libmad              0.15.1b    http://www.underbit.com/products/mad/
+  libogg              1.3.5      http://www.vorbis.com/
+  libvorbis           1.3.7      http://www.vorbis.com/
+  flac                1.3.3      https://xiph.org/flac/index.html
+  wavpack             5.4.0      http://www.wavpack.com/
+  libsndfile          1.0.31     http://www.mega-nerd.com/libsndfile/
+  opus                1.3.1      http://www.opus-codec.org/
+  opusfile            0.12       http://www.opus-codec.org/
+  libbs2b             3.1.0      http://bs2b.sourceforge.net/
+  curl                7.77.0     http://curl.haxx.se/
+  libcdio             2.1.0      http://www.gnu.org/software/libcdio/
+  libcdio-paranoia    10.2+2.0.1 http://www.gnu.org/software/libcdio/  
+  mingw-libgnurx      2.5.1      http://sourceforge.net/projects/mingw/files/Other/UserContributed/regex/
+  libcddb             1.3.2      http://libcddb.sourceforge.net/
+  ffmpeg              3.4.8      http://ffmpeg.org/
+  libmodplug          0.8.9.0    http://modplug-xmms.sourceforge.net/
+  game-music-emu      0.6.3      https://bitbucket.org/mpyne/game-music-emu
+  musepack            r475       http://www.musepack.net/
+  glew                2.2.0      http://glew.sourceforge.net/
+  projectm            2.1.0      http://projectm.sourceforge.net/
+  libsidplayfp        2.1.2      http://sourceforge.net/projects/sidplay-residfp/
+  enca                1.19       http://cihar.com/software/enca/
+  soxr                0.1.3      https://sourceforge.net/projects/soxr/
+  libxml2             2.9.12     http://xmlsoft.org/
+  librcd              0.1.14     https://github.com/RusXMMS/librcd
+  mpg123              1.27.2     https://www.mpg123.de/
+  adplug              2.3.3      https://github.com/adplug/adplug
+  libbinio            1.5        https://github.com/adplug/libbinio
+
+
+List of the third-party artwork.
+
+  Name                Web page
+
+  Oxygen icons        https://community.kde.org/Frameworks
+  Bunjee skin         http://gnome-look.org/content/show.php?content=71828
+  Vortigo skin        http://gnome-look.org/content/show.php?content=55440
+  Azenis skin         http://packages.altlinux.org/ru/Sisyphus/srpms/qmmp-skins-azenis
+
+Patches and build scripts: https://sourceforge.net/p/qmmp-dev/code/HEAD/tree/trunk/extras/package/win32/
