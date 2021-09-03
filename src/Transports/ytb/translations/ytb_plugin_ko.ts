0adafab06d5dc1478f8dea654532b952c06382bf
trialuser02 <trialuser02>
2021-09-03 18:56:45 +0000

    updated .ts files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-plugin-pack-2.1@10276 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Transports/ytb/translations/ytb_plugin_ko.ts
+++ src/Transports/ytb/translations/ytb_plugin_ko.ts
@@ -9,32 +9,27 @@
         <translation>Youtube 플러그인</translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="60"/>
-        <source>not found</source>
-        <translation>찾을 수 없음</translation>
-    </message>
-    <message>
-        <location filename="../ytbinputfactory.cpp" line="62"/>
+        <location filename="../ytbinputfactory.cpp" line="69"/>
         <source>About Youtube Transport Plugin</source>
         <translation>Youtube 전송 플러그인 정보</translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="63"/>
+        <location filename="../ytbinputfactory.cpp" line="70"/>
         <source>Qmmp Youtube Transport Plugin</source>
         <translation>Qmmp Youtube 전송 플러그인</translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="64"/>
-        <source>This plugin adds feature to play audio from Youtube using &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt; utility</source>
-        <translation>이 플러그인은 &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt; 유틸리티를 사용하여 Youtube의 오디오를 재생할 수 있는 기능을 추가합니다</translation>
+        <location filename="../ytbinputfactory.cpp" line="71"/>
+        <source>This plugin adds feature to play audio from Youtube using %1 utility</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="66"/>
-        <source>youtube-dl version: %1</source>
-        <translation>youtube-dl 버전: %1</translation>
+        <location filename="../ytbinputfactory.cpp" line="73"/>
+        <source>%1 version: %2</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="67"/>
+        <location filename="../ytbinputfactory.cpp" line="74"/>
         <source>Written by: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</source>
         <translation>작성자: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</translation>
     </message>
@@ -42,17 +37,17 @@
 <context>
     <name>YtbInputSource</name>
     <message>
-        <location filename="../ytbinputsource.cpp" line="154"/>
+        <location filename="../ytbinputsource.cpp" line="184"/>
         <source>Uploader</source>
         <translation>업로드 도구</translation>
     </message>
     <message>
-        <location filename="../ytbinputsource.cpp" line="155"/>
+        <location filename="../ytbinputsource.cpp" line="185"/>
         <source>Upload date</source>
         <translation>업로드 날짜</translation>
     </message>
     <message>
-        <location filename="../ytbinputsource.cpp" line="156"/>
+        <location filename="../ytbinputsource.cpp" line="186"/>
         <source>Duration</source>
         <translation>재생시간</translation>
     </message>
