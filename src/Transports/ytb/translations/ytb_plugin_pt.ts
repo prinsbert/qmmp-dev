c08500894a6347047127f6efd2ae1accd074e9cb
trialuser02 <trialuser02>
2021-09-03 18:55:43 +0000

    updated .ts files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp-plugin-pack@10275 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Transports/ytb/translations/ytb_plugin_pt.ts
+++ src/Transports/ytb/translations/ytb_plugin_pt.ts
@@ -9,32 +9,27 @@
         <translation>Suplemento Youtube</translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="60"/>
-        <source>not found</source>
-        <translation>não encontrado</translation>
-    </message>
-    <message>
-        <location filename="../ytbinputfactory.cpp" line="62"/>
+        <location filename="../ytbinputfactory.cpp" line="69"/>
         <source>About Youtube Transport Plugin</source>
         <translation>Acerca de suplemento Youtube Transport</translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="63"/>
+        <location filename="../ytbinputfactory.cpp" line="70"/>
         <source>Qmmp Youtube Transport Plugin</source>
         <translation>Suplemento Qmmp Youtube Transport</translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="64"/>
-        <source>This plugin adds feature to play audio from Youtube using &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt; utility</source>
-        <translation>Este suplemento adiciona a possibilidade de reproduzir o áudio dos vídeos do Youtube através do utilitário &lt;a href=&quot;https://youtube-dl.org/&quot;&gt;youtube-dl&lt;/a&gt;</translation>
+        <location filename="../ytbinputfactory.cpp" line="71"/>
+        <source>This plugin adds feature to play audio from Youtube using %1 utility</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="66"/>
-        <source>youtube-dl version: %1</source>
-        <translation>youtube-dl, versão: %1</translation>
+        <location filename="../ytbinputfactory.cpp" line="73"/>
+        <source>%1 version: %2</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ytbinputfactory.cpp" line="67"/>
+        <location filename="../ytbinputfactory.cpp" line="74"/>
         <source>Written by: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</source>
         <translation>Desenvolvido: Ilya Kotov &amp;lt;forkotov02@ya.ru&amp;gt;</translation>
     </message>
@@ -42,17 +37,17 @@
 <context>
     <name>YtbInputSource</name>
     <message>
-        <location filename="../ytbinputsource.cpp" line="154"/>
+        <location filename="../ytbinputsource.cpp" line="184"/>
         <source>Uploader</source>
         <translation>Publicador</translation>
     </message>
     <message>
-        <location filename="../ytbinputsource.cpp" line="155"/>
+        <location filename="../ytbinputsource.cpp" line="185"/>
         <source>Upload date</source>
         <translation>Data de publicação</translation>
     </message>
     <message>
-        <location filename="../ytbinputsource.cpp" line="156"/>
+        <location filename="../ytbinputsource.cpp" line="186"/>
         <source>Duration</source>
         <translation>Duração</translation>
     </message>
