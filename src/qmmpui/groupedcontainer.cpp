2e42caf8c41f4817b152f3ea4b72ad2c07e773cd
trialuser02 <trialuser02>
2022-01-16 20:05:11 +0000

    fixed drag-and-dropped tracks order (#1101)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.5@10462 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/groupedcontainer.cpp
+++ src/qmmpui/groupedcontainer.cpp
@@ -90,7 +90,7 @@ void GroupedContainer::addTracks(const QList<PlayListTrack *> &tracks)
     m_update = true;
 }
 
-void GroupedContainer::insertTrack(int index, PlayListTrack *track)
+int GroupedContainer::insertTrack(int index, PlayListTrack *track)
 {
     int firstIndex = 0, lastIndex = 0;
     //insert if possible
@@ -112,11 +112,12 @@ void GroupedContainer::insertTrack(int index, PlayListTrack *track)
         {
             m_groups[i]->trackList.insert(index - firstIndex - 1, track);
             m_update = true;
-            return;
+            return index;
         }
     }
     //just add otherwise
     addTrack(track);
+    return m_items.count() - 1;
 }
 
 void GroupedContainer::replaceTracks(const QList<PlayListTrack *> &tracks)
