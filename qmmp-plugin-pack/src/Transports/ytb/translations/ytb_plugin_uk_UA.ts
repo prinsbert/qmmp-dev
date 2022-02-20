38f5916a600b678a569ba633b7aa846641b60469
motsyo <motsyo>
2022-02-20 19:38:50 +0000

    update Ukrainian translation
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@10500 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/src/Transports/ytb/translations/ytb_plugin_uk_UA.ts
+++ qmmp-plugin-pack/src/Transports/ytb/translations/ytb_plugin_uk_UA.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../ytbinputfactory.cpp" line="71"/>
         <source>This plugin adds feature to play audio from Youtube using %1 utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль додає функцію відтворення аудіо з Youtube за допомогою утиліти %1</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="73"/>
         <source>%1 version: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 версія: %2</translation>
     </message>
     <message>
         <location filename="../ytbinputfactory.cpp" line="74"/>
