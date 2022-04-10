75e6098c2dd6732b16bd320a82d7466bf83c5511
trialuser02 <trialuser02>
2022-04-10 10:48:14 +0000

    updated translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp@10571 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/qsui/translations/qsui_plugin_pl_PL.ts
+++ src/plugins/Ui/qsui/translations/qsui_plugin_pl_PL.ts
@@ -991,7 +991,7 @@
     <message>
         <location filename="../playlistbrowser.cpp" line="62"/>
         <source>Quick Search</source>
-        <translation type="unfinished">Szybkie wyszukiwanie</translation>
+        <translation>Szybkie wyszukiwanie</translation>
     </message>
 </context>
 <context>
