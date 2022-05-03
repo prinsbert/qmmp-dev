e329fe1d17d269b1125c71eeee8ce24163683196
trialuser02 <trialuser02>
2022-05-03 14:03:31 +0000

    win32 toolchain: curl-7.83.0, libsndfile-1.1.0
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@10589 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/curl/curl.sh
+++ extras/package/win32/contrib/curl/curl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NAME=curl
-VERSION=7.82.0
+VERSION=7.83.0
 
 case $1 in
   --download)
--- extras/package/win32/contrib/libsndfile/libsndfile.sh
+++ extras/package/win32/contrib/libsndfile/libsndfile.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 NAME=libsndfile
-VERSION=1.0.31
+VERSION=1.1.0
 
 case $1 in
   --download)
     mkdir -p temp
     cd temp
-    wget -nc https://github.com/libsndfile/libsndfile/releases/download/$VERSION/$NAME-$VERSION.tar.bz2
+    wget -nc https://github.com/libsndfile/libsndfile/releases/download/$VERSION/$NAME-$VERSION.tar.xz
   ;;
   --install)
     cd temp
-    tar xvjf $NAME-$VERSION.tar.bz2
+    tar xvJf $NAME-$VERSION.tar.xz
     cd $NAME-$VERSION
     ./configure --prefix=$PREFIX --enable-shared --disable-static --disable-external-libs --disable-sqlite
     make -j${JOBS}
