d577f3409d6b14d79cf43c6ebaa220d8858828eb
trialuser02 <trialuser02>
2022-05-10 14:25:58 +0000

    updated translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp-plugin-pack@10593 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Effect/srconverter/translations/srconverter_plugin_fr.ts
+++ src/Effect/srconverter/translations/srconverter_plugin_fr.ts
@@ -34,12 +34,12 @@
     <message>
         <location filename="../settingsdialog.ui" line="19"/>
         <source>Sample Rate (Hz):</source>
-        <translation>Taux d&apos;échantillonnage (Hz) :</translation>
+        <translation>Taux d&apos;échantillonnage (Hz)&#xa0;:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="39"/>
         <source>Interpolation Engine:</source>
-        <translation>Moteur d&apos;interpolation :</translation>
+        <translation>Moteur d&apos;interpolation&#xa0;:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="47"/>
