109203737377d25673b246baf334275c030f3e9c
trialuser02 <trialuser02>
2022-05-10 14:49:10 +0000

    updated Russian translation (Andrei Stepanov)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@10594 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/qmmpui/translations/libqmmpui_ru.ts
+++ qmmp/src/qmmpui/translations/libqmmpui_ru.ts
@@ -353,7 +353,7 @@
     <message>
         <location filename="../forms/configdialog.ui" line="229"/>
         <source>Skip already existing tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Пропустить существующие дорожки</translation>
     </message>
     <message>
         <location filename="../forms/configdialog.ui" line="277"/>
@@ -915,7 +915,7 @@
     <message>
         <location filename="../detailsdialog.cpp" line="284"/>
         <source>Lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>Текст песни</translation>
     </message>
     <message>
         <location filename="../detailsdialog.cpp" line="304"/>
