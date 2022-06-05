af94cb0466dbc33aeb8c3cfac8c7331d964d2411
trialuser02 <trialuser02>
2022-06-05 16:01:04 +0000

    updated translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.6@10719 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/lyrics/translations/lyrics_plugin_ko.ts
+++ src/plugins/General/lyrics/translations/lyrics_plugin_ko.ts
@@ -123,7 +123,7 @@
     <message>
         <location filename="../lyricswidget.cpp" line="253"/>
         <source>Cache</source>
-        <translation type="unfinished"></translation>
+        <translation>캐시</translation>
     </message>
 </context>
 <context>
