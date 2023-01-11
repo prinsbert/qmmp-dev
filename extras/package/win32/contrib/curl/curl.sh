499a814b473b77f79f7be6dd922385d3625f169d
trialuser02 <trialuser02>
2023-01-11 20:54:32 +0000

    win32 toolchain: curl-7.87.0, libsndfile-1.2.0
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@10874 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/curl/curl.sh
+++ extras/package/win32/contrib/curl/curl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NAME=curl
-VERSION=7.86.0
+VERSION=7.87.0
 
 case $1 in
   --download)
--- extras/package/win32/contrib/libsndfile/libsndfile.sh
+++ extras/package/win32/contrib/libsndfile/libsndfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NAME=libsndfile
-VERSION=1.1.0
+VERSION=1.2.0
 
 case $1 in
   --download)
