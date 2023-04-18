527aed23a1243d3d48354519da46700e1f413d95
trialuser02 <trialuser02>
2023-04-18 12:37:59 +0000

    win32 toolchain: qt-5.15.9
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@11016 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/qtbase/qtbase.sh
+++ extras/package/win32/contrib/qtbase/qtbase.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NAME=qtbase-everywhere-opensource-src
-VERSION=5.15.8
+VERSION=5.15.9
 BUILD_ROOT=qtbase-everywhere-src-$VERSION
 
 case $1 in
--- extras/package/win32/contrib/qttranslations/qttranslations.sh
+++ extras/package/win32/contrib/qttranslations/qttranslations.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NAME=qttranslations-everywhere-opensource-src
-VERSION=5.15.8
+VERSION=5.15.9
 BUILD_ROOT=qttranslations-everywhere-src-$VERSION
 
 case $1 in
