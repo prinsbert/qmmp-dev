410d1d1324c7503b7d0811a02a79aaf7fc1da5c5
trialuser02 <trialuser02>
2023-07-14 21:28:00 +0000

    added some legacy stuff
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@11128 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ legacy/win32-contrib/libxml2/libxml2.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+NAME=libxml2
+VERSION=2.11.4
+
+case $1 in
+  --download)
+    mkdir -p temp
+    cd temp
+    wget --no-check-certificate -nc https://download.gnome.org/sources/libxml2/2.11/$NAME-$VERSION.tar.xz
+  ;;
+  --install)
+    cd temp
+    tar xvJf $NAME-$VERSION.tar.xz
+    cd $NAME-$VERSION
+    ./configure --prefix=$PREFIX --enable-shared --disable-static --without-http --without-debug --without-docbook \
+    --without-html --without-modules --without-python --with-zlib=no  --without-html --without-ftp
+    make -j${JOBS}
+    make install
+
+  ;;
+  --clean)
+    cd temp
+    rm -rf $NAME-$VERSION
+    cd ..
+  ;;
+esac
---
+++ legacy/win32-contrib/taglib-rusxmms/taglib-rusxmms.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+NAME=taglib
+VERSION=1.13
+
+case $1 in
+  --download)
+    mkdir -p temp
+    cd temp
+    wget -nc --no-check-certificate https://github.com/taglib/taglib/releases/download/v$VERSION/$NAME-$VERSION.tar.gz \
+    -O $NAME-$VERSION.tar.gz
+    #wget -nc http://darksoft.org/files/rusxmms/patches/taglib-csa10.tar.bz2
+  ;;
+  --install)
+    cd temp
+    tar xvzf $NAME-$VERSION.tar.gz
+    cd $NAME-$VERSION
+    cat ../../taglib-1.13-ds-rusxmms.patch | patch -p1 --verbose
+    cmake ./ -DCMAKE_INSTALL_PREFIX=${PREFIX}/taglib-rusxmms -G "MSYS Makefiles" -DZLIB_ROOT=${ZLIB_ROOT} \
+    -DCMAKE_COLOR_MAKEFILE:BOOL=OFF -DCMAKE_CXX_FLAGS="-I${PREFIX}/include" -DCMAKE_SHARED_LINKER_FLAGS="-L${PREFIX}/lib" \
+    -DBUILD_SHARED_LIBS=ON
+    mingw32-make -j${JOBS}
+    mingw32-make install
+
+  ;;
+  --clean)
+    cd temp
+    rm -rf $NAME-$VERSION
+    cd ..
+  ;;
+esac
