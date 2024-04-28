b6b773d2d39de02d2f16e1dbb429bc55b7400dce
trialuser02 <trialuser02>
2024-04-28 12:23:13 +0000

    fixed memory leak
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.6@11455 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/streambrowser/streamwindow.cpp
+++ src/plugins/General/streambrowser/streamwindow.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2012-2022 by Ilya Kotov                                 *
+ *   Copyright (C) 2012-2024 by Ilya Kotov                                 *
  *   forkotov02@ya.ru                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -147,7 +147,9 @@ StreamWindow::StreamWindow(QWidget *parent)
 }
 
 StreamWindow::~StreamWindow()
-{}
+{
+    delete m_ui;
+}
 
 void StreamWindow::showText(QNetworkReply *reply)
 {
