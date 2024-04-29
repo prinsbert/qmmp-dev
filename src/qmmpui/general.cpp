b343383e190fb994791636c1e99e7b5e6bcceba6
trialuser02 <trialuser02>
2024-04-29 19:22:46 +0000

    libqmmpui: fixed unexpected startup of the disabled plugin when trying to change its settings
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp@11479 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/general.cpp
+++ src/qmmpui/general.cpp
@@ -199,14 +199,16 @@ void General::showSettings(GeneralFactory *factory, QWidget *parentWidget)
     if(m_generals && dialog->exec() == QDialog::Accepted)
     {
         if(m_generals->contains(factory))
+        {
             delete m_generals->take(factory);
 
-        QObject *general = factory->create(m_parent);
-        if(general)
-            m_generals->insert(factory, general);
+            QObject *general = factory->create(m_parent);
+            if(general)
+                m_generals->insert(factory, general);
 
-        for(const WidgetDescription &d : factory->properties().widgets)
-            emit UiHelper::instance()->widgetUpdated(QStringLiteral("%1_%2").arg(factory->properties().shortName).arg(d.id));
+            for(const WidgetDescription &d : factory->properties().widgets)
+                emit UiHelper::instance()->widgetUpdated(QStringLiteral("%1_%2").arg(factory->properties().shortName).arg(d.id));
+        }
     }
     dialog->deleteLater();
 }
