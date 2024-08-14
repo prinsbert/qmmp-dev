fa39111393b5201524293e79a082dcda4effddf8
trialuser02 <trialuser02>
2024-08-14 15:21:35 +0000

    updated .ts files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp@11742 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/kdenotify/translations/kdenotify_plugin_pt_BR.ts
+++ src/plugins/General/kdenotify/translations/kdenotify_plugin_pt_BR.ts
@@ -31,8 +31,8 @@
     <name>KdeNotifySettingsDialog</name>
     <message>
         <location filename="../kdenotifysettingsdialog.ui" line="14"/>
-        <source>KDE 4 Notification Plugin Settings</source>
-        <translation type="unfinished">Preferências do plugin KDE 4 Notification </translation>
+        <source>KDE Notification Plugin Settings</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../kdenotifysettingsdialog.ui" line="39"/>
