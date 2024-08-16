e1ec4ef7a88e78530b82113a0cadd98305d9d8c9
trialuser02 <trialuser02>
2024-08-16 17:19:10 +0000

    updated translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.7@11767 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/cdaudio/translations/cdaudio_plugin_pl_PL.ts
+++ src/plugins/Input/cdaudio/translations/cdaudio_plugin_pl_PL.ts
@@ -79,7 +79,7 @@
     <message>
         <location filename="../decodercdaudiofactory.cpp" line="104"/>
         <source>Compiled against libcdio-%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Skompilowana z biblioteką libcdio-%1</translation>
     </message>
     <message>
         <location filename="../decodercdaudiofactory.cpp" line="107"/>
