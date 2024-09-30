513b310bd75fe2217e438ceba358a95ad22c30ac
trialuser02 <trialuser02>
2024-09-30 20:38:55 +0000

    fixed Qt6 localization bug
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@11851 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/qtbase-qt6/qtbase-qt6.sh
+++ extras/package/win32/contrib/qtbase-qt6/qtbase-qt6.sh
@@ -14,6 +14,7 @@ case $1 in
     cd temp
     tar xvJf $NAME-$VERSION.tar.xz -C $DEV_PATH
     cp ../build.bat $DEV_PATH/$BUILD_ROOT
+    cat ../QTBUG-129434.patch | patch -p1 -d $DEV_PATH/$BUILD_ROOT
     cd $DEV_PATH/$BUILD_ROOT
     cmd /c build.bat
     cmake --build . --parallel ${JOBS}
