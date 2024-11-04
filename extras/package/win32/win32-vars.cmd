611a28238f93c7b165bdfc4a6b7978a90b6ce8f3
trialuser02 <trialuser02>
2024-11-04 18:25:14 +0000

    win32 toolchain: fixed build environment
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@11991 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/win32-vars.cmd
+++ extras/package/win32/win32-vars.cmd
@@ -1,2 +1,2 @@
-SET PATH=C:\devel\msys\bin;C:\devel\mingw32\bin;C:\devel\qt6\bin;C:\devel\mingw32-libs\bin;%PATH%
+SET PATH=C:\devel\msys\bin;C:\devel\mingw32\bin;C:\devel\mingw32\opt\bin;C:\devel\qt6\bin;C:\devel\mingw32-libs\bin;%PATH%
 cmd
