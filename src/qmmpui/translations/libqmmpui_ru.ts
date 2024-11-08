9e3cf0aa7763c5b30190af6d92946da89cdfa8d1
trialuser02 <trialuser02>
2024-11-08 22:03:00 +0000

    updated translations
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-2.2@12015 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/translations/libqmmpui_ru.ts
+++ src/qmmpui/translations/libqmmpui_ru.ts
@@ -86,22 +86,22 @@
     <message>
         <location filename="../aboutdialog.cpp" line="73"/>
         <source>Qt version: %1 (compiled with %2)</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия Qt: %1 (собрано с %2)</translation>
     </message>
     <message>
         <location filename="../aboutdialog.cpp" line="74"/>
         <source>Qt platform: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Платформа Qt: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog.cpp" line="75"/>
         <source>System: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Система: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog.cpp" line="76"/>
         <source>Build ABI: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Бинарный интерфейс сборки: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog.cpp" line="113"/>
