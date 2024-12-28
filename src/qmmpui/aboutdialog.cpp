bc983a052212f3e5685e2c366fbfbac3bb027e90
trialuser02 <trialuser02>
2024-12-28 20:30:52 +0000

    updated copyright
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.12@12103 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/aboutdialog.cpp
+++ src/qmmpui/aboutdialog.cpp
@@ -1,5 +1,5 @@
 /**************************************************************************
-*   Copyright (C) 2006-2023 by Ilya Kotov                                 *
+*   Copyright (C) 2006-2025 by Ilya Kotov                                 *
 *   forkotov02@ya.ru                                                      *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -68,7 +68,7 @@ QString AboutDialog::loadAbout()
     text.append(tr("Using Qt %1 (compiled with Qt %2)" ).arg(qVersion()).arg(QT_VERSION_STR) + "</p>");
 
     text.append("<p>");
-    text.append(tr("(c) %1-%2 Qmmp Development Team").arg(2006).arg(2024)+"<br>");
+    text.append(tr("(c) %1-%2 Qmmp Development Team").arg(2006).arg(2025)+"<br>");
     text.append("<a href=\"https://qmmp.ylsoftware.com/\">https://qmmp.ylsoftware.com/</a><br>");
     text.append("<a href=\"https://sourceforge.net/projects/qmmp-dev/\">https://sourceforge.net/projects/qmmp-dev/</a>");
     text.append("</p>");
