ac60af3bbc24090165b2a1646269dea76729cc0a
trialuser02 <trialuser02>
2024-12-28 21:07:15 +0000

    updated copyright
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.7@12106 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Effect/crossfade/crossfadeplugin.cpp
+++ src/plugins/Effect/crossfade/crossfadeplugin.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009-2024 by Ilya Kotov <forkotov02@ya.ru>              *
+ *   Copyright (C) 2009-2025 by Ilya Kotov <forkotov02@ya.ru>              *
  *   Copyright (C) 2009 by Sebastian Pipping <sebastian@pipping.org>       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
