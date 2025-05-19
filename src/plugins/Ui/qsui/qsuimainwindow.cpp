ba51f0d2959be4dddc8e17d657157b9aa7449126
trialuser02 <trialuser02>
2025-05-19 16:34:49 +0000

    qsui: fixed issue with hidden menubar
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.7@12457 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/qsui/qsuimainwindow.cpp
+++ src/plugins/Ui/qsui/qsuimainwindow.cpp
@@ -413,10 +413,8 @@ QMenu *QSUiMainWindow::createPopupMenu()
 {
     QMenu *menu = QMainWindow::createPopupMenu();
     menu->addSeparator();
-    QAction *menuBarAction = menu->addAction(tr("Menu Bar"));
-    menuBarAction->setCheckable(true);
-    menuBarAction->setChecked(menuBar()->isVisible());
-    connect(menuBarAction, &QAction::toggled, menuBar(), &QMenuBar::setVisible);
+    menu->addAction(m_menuBarAction);
+    m_menuBarAction->setChecked(menuBar()->isVisible());
     return menu;
 }
 
@@ -639,6 +637,10 @@ void QSUiMainWindow::createActions()
     connect(backwardAction, &QAction::triggered, this, &QSUiMainWindow::backward);
     //application menu
     SET_ACTION(QSUiActionManager::APPLICATION_MENU, this, &QSUiMainWindow::showAppMenu);
+    //menu bar
+    m_menuBarAction = new QAction(tr("Menu Bar"), this);
+    m_menuBarAction->setCheckable(true);
+    connect(m_menuBarAction, &QAction::triggered, menuBar(), &QMenuBar::setVisible);
 
     addActions({ forwardAction, backwardAction });
     addActions(QSUiActionManager::instance()->actions());
@@ -706,7 +708,8 @@ void QSUiMainWindow::readSettings()
     else
     {
         restoreGeometry(settings.value(u"mw_geometry"_s).toByteArray());
-        menuBar()->setVisible(settings.value(u"show_menubar"_s, true).toBool());
+        m_menuBarAction->setChecked(settings.value(u"show_menubar"_s, true).toBool());
+        menuBar()->setVisible(m_menuBarAction->isChecked());
 
         QByteArray wstate = settings.value(u"mw_state"_s).toByteArray();
         if(wstate.isEmpty())
@@ -813,7 +816,7 @@ void QSUiMainWindow::writeSettings()
     settings.setValue(u"Simple/show_tabs"_s, ACTION(QSUiActionManager::UI_SHOW_TABS)->isChecked());
     settings.setValue(u"Simple/block_dockwidgets"_s, ACTION(QSUiActionManager::UI_BLOCK_DOCKWIDGETS)->isChecked());
     settings.setValue(u"Simple/block_toolbars"_s, ACTION(QSUiActionManager::UI_BLOCK_TOOLBARS)->isChecked());
-    settings.setValue(u"Simple/show_menubar"_s, menuBar()->isVisible());
+    settings.setValue(u"Simple/show_menubar"_s, m_menuBarAction->isChecked() );
 }
 
 void QSUiMainWindow::savePlayList()
