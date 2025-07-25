8744fb98cdbaa31c88bcd7fb0279ab3ecf2b0626
trialuser02 <trialuser02>
2025-07-25 11:21:06 +0000

    mpg123: fixed download url
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@12559 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/mpg123/mpg123.sh
+++ extras/package/win32/contrib/mpg123/mpg123.sh
@@ -7,7 +7,7 @@ case $1 in
   --download)
     mkdir -p temp
     cd temp
-    wget -nc https://www.mpg123.de/download/$NAME-$VERSION.tar.bz2
+    wget -nc https://sourceforge.net/projects/$NAME/files/$NAME/$VERSION/$NAME-$VERSION.tar.bz2
   ;;
   --install)
     cd temp
