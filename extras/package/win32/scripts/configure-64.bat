bc031dd3b3cdf0990e92d18d80bba579e24ab440
trialuser02 <trialuser02>
2025-08-29 19:15:56 +0000

    win32 toolchain: fixed configure scripts
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@12681 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/scripts/configure-64.bat
+++ extras/package/win32/scripts/configure-64.bat
@@ -1,3 +1,4 @@
+SET PKG_CONFIG_PATH=C:\devel\mingw64-libs\lib\pkgconfig;C:\devel\qmmp-win64\lib\pkgconfig;;C:\devel\qt6-win64\lib\pkgconfig
 cmake . -G "MSYS Makefiles" -GNinja -DUSE_LIBRCD=ON ^
 -DCMAKE_REQUIRED_INCLUDES=C:\devel\mingw64-libs\include ^
 -DCMAKE_SYSTEM_LIBRARY_PATH=C:\devel\mingw64-libs\lib ^
