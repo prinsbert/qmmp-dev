1f05e469fac0da7ab804ccc4b24ee89f23f1c30a
trialuser02 <trialuser02>
2025-08-29 18:59:39 +0000

    fixed issues with "--pl-help" and "--nowplaying-syntax" command line options
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@12680 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/app/qmmpstarter.cpp
+++ qmmp/src/app/qmmpstarter.cpp
@@ -148,7 +148,7 @@ QMMPStarter::QMMPStarter() : QObject()
     for(auto it = commands.cbegin(); it != commands.cend(); ++it)
     {
         CommandLineHandler::OptionFlags flags;
-        if(!m_option_manager->identify(it.key()) && !CommandLineManager::hasOption(it.key(), &flags) &&
+        if(m_option_manager->identify(it.key()) < 0 && !CommandLineManager::hasOption(it.key(), &flags) &&
                 it.key() != u"--no-start"_s && it.key() != u"--ui"_s)
         {
             cout << qPrintable(tr("Unknown command")) << endl;
