e490209f7f9c0d51e3dfe9e0cf26763fd83848b4
trialuser02 <trialuser02>
2025-09-20 19:42:49 +0000

    kdenotify: fixed debug messages
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@12731 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/kdenotify/kdenotify.cpp
+++ qmmp/src/plugins/General/kdenotify/kdenotify.cpp
@@ -44,11 +44,11 @@ KdeNotify::KdeNotify(QObject *parent) : QObject(parent)
                                     QDBusConnection::sessionBus(), this);
     if(m_notifier->lastError().type() != QDBusError::NoError)
     {
-        qCWarning(plugin) << "KdeNotify: Unable to create interface:" << m_notifier->lastError().message();
+        qCWarning(plugin) << "Unable to create interface:" << m_notifier->lastError().message();
         return;
     }
 
-    qCWarning(plugin) << "KdeNotify: DBus interfece created successfully.";
+    qCDebug(plugin) << "DBus interface created successfully.";
     QDir dir(Qmmp::cacheDir());
     if(!dir.exists(u"kdenotifycache"_s))
         dir.mkdir(u"kdenotifycache"_s);
