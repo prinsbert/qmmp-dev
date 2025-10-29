c0a85c1dcff79ccd0cb836d76ffb678cbff0e868
trialuser02 <trialuser02>
2025-10-29 09:15:39 +0000

    sndfile: added IMA ADPCM support
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-2.3@12761 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/sndfile/decodersndfilefactory.cpp
+++ src/plugins/Input/sndfile/decodersndfilefactory.cpp
@@ -39,6 +39,7 @@
 #define WAVE_FORMAT_IEEE_FLOAT 0x0003
 #define WAVE_FORMAT_ALAW 0x0006
 #define WAVE_FORMAT_MULAW 0x0007
+#define WAVE_FORMAT_IMA_ADPCM 0x0011
 #define WAVE_FORMAT_EXTENSIBLE 0xfffe
 
 inline QMap<Qmmp::MetaData, QString> id3v2toMetaData(TagLib::ID3v2::Tag *tag)
@@ -123,6 +124,7 @@ bool DecoderSndFileFactory::canDecode(QIODevice *input) const
         case WAVE_FORMAT_IEEE_FLOAT:
         case WAVE_FORMAT_ALAW:
         case WAVE_FORMAT_MULAW:
+        case WAVE_FORMAT_IMA_ADPCM:
         case WAVE_FORMAT_EXTENSIBLE:
             return true;
         default:
