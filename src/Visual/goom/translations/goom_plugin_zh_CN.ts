904caed8c52a801eebfc7447e11be9a16962d308
trialuser02 <trialuser02>
2025-11-23 10:51:42 +0000

    updated Chinese Simplified translation (Rain Ding)
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-plugin-pack-2.3@12771 90c681e8-e032-0410-971d-27865f9a5e38

--- src/Visual/goom/translations/goom_plugin_zh_CN.ts
+++ src/Visual/goom/translations/goom_plugin_zh_CN.ts
@@ -66,7 +66,7 @@
     <message>
         <location filename="../visualgoomfactory.cpp" line="51"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>作者：Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
     </message>
     <message>
         <location filename="../visualgoomfactory.cpp" line="52"/>
