66808fa60d78d6d89a7667f21c9db4aaf3b70aca
trialuser02 <trialuser02>
2026-03-18 20:05:55 +0000

    EqSettings: fixed copy operator
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-0.12@12933 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/eqsettings.cpp
+++ src/qmmp/eqsettings.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2010-2019 by Ilya Kotov                                 *
+ *   Copyright (C) 2010-2026 by Ilya Kotov                                 *
  *   forkotov02@ya.ru                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -81,7 +81,7 @@ void EqSettings::setPreamp(double preamp)
 
 EqSettings &EqSettings::operator=(const EqSettings &s)
 {
-    for(int i = 0; i < m_bands; ++i)
+    for(int i = 0; i < s.m_bands; ++i)
         m_gains[i] = s.m_gains[i];
     m_preamp = s.m_preamp;
     m_is_enabled = s.m_is_enabled;
