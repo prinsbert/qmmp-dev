8e834201fdb14a65d95ea0b4e439e0997175b2b2
trialuser02 <trialuser02>
2026-04-14 20:43:39 +0000

    updated Russian translation
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@13042 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/batchtageditor/translations/batchtageditor_plugin_ru.ts
+++ qmmp/src/plugins/General/batchtageditor/translations/batchtageditor_plugin_ru.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.1" language="ru">
 <context>
     <name>BatchTagEditor</name>
     <message>
         <location filename="../batchtageditor.cpp" line="42"/>
         <source>Change Tags</source>
-        <translation type="unfinished"></translation>
+        <translation>Изменить теги</translation>
     </message>
     <message>
         <location filename="../batchtageditor.cpp" line="70"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Предупреждение</translation>
     </message>
     <message>
         <location filename="../batchtageditor.cpp" line="70"/>
         <source>Unable to find editable tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Не найдены треки с возможностью редактирования</translation>
     </message>
 </context>
 <context>
@@ -24,57 +24,57 @@
     <message>
         <location filename="../batchtageditordialog.ui" line="14"/>
         <source>Change Tags of the Selected Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Изменить теги выбранных файлов</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="16"/>
         <source>Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Исполнитель</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="17"/>
         <source>Album Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Исполнитель альбома</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="18"/>
         <source>Album</source>
-        <translation type="unfinished"></translation>
+        <translation>Альбом</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="19"/>
         <source>Comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Комментарий</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="20"/>
         <source>Genre</source>
-        <translation type="unfinished"></translation>
+        <translation>Жанр</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="21"/>
         <source>Composer</source>
-        <translation type="unfinished"></translation>
+        <translation>Композитор</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="22"/>
         <source>Year</source>
-        <translation type="unfinished"></translation>
+        <translation>Год</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="23"/>
         <source>Disc Number</source>
-        <translation type="unfinished"></translation>
+        <translation>Номер диска</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="79"/>
         <source>Writing tags...</source>
-        <translation type="unfinished"></translation>
+        <translation>Запись тегов...</translation>
     </message>
     <message>
         <location filename="../batchtageditordialog.cpp" line="79"/>
         <source>Interrupt</source>
-        <translation type="unfinished"></translation>
+        <translation>Прервать</translation>
     </message>
 </context>
 <context>
@@ -82,27 +82,27 @@
     <message>
         <location filename="../batchtageditorfactory.cpp" line="28"/>
         <source>Tag Editor Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль редакт. тегов</translation>
     </message>
     <message>
         <location filename="../batchtageditorfactory.cpp" line="48"/>
         <source>About Tag Editor Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>О модуле редактирования тегов</translation>
     </message>
     <message>
         <location filename="../batchtageditorfactory.cpp" line="49"/>
         <source>Qmmp Tag Editor Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль редактирования тегов для Qmmp</translation>
     </message>
     <message>
         <location filename="../batchtageditorfactory.cpp" line="50"/>
         <source>This plugin allows for batch editing of tags</source>
-        <translation type="unfinished"></translation>
+        <translation>Данные модуль позволяет массово редактировать теги</translation>
     </message>
     <message>
         <location filename="../batchtageditorfactory.cpp" line="51"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@ya.ru&gt;</translation>
     </message>
 </context>
 </TS>
