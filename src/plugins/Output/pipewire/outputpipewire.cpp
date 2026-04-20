994a9315f129124fec358d6848beccdf1121e3e6
trialuser02 <trialuser02>
2026-04-20 15:39:59 +0000

    fixed build warnings
    git-svn-id: file:///home/maarten/qmmp-svn-raw/branches/qmmp-1.7@13060 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Output/pipewire/outputpipewire.cpp
+++ src/plugins/Output/pipewire/outputpipewire.cpp
@@ -32,6 +32,7 @@
 #include <time.h>
 static uint64_t pw_stream_get_nsec(struct pw_stream *stream)
 {
+    Q_UNUSED(stream);
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return SPA_TIMESPEC_TO_NSEC(&ts);
@@ -42,6 +43,7 @@ static uint64_t pw_stream_get_nsec(struct pw_stream *stream)
 static inline int pw_stream_get_time_n(struct pw_stream * stream,
                                        struct pw_time * time, size_t size)
 {
+    Q_UNUSED(size);
     return pw_stream_get_time(stream, time);
 }
 #endif
