1719cacd7834db799b94d69a05c86340624fc048
trialuser02 <trialuser02>
2026-04-24 20:18:44 +0000

    updated .ts files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp-plugin-pack@13087 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/Input/timidity/translations/timidity_plugin_en.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>DecoderTiMidityFactory</name>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="41"/>
+        <source>TiMidity Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="43"/>
+        <source>Midi Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="109"/>
+        <source>About TiMidity Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="110"/>
+        <source>Qmmp TiMidity Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="111"/>
+        <source>This plugin uses libTiMidity library to play midi files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="112"/>
+        <source>Compiled against libTiMidity-%1.%2.%3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodertimidityfactory.cpp" line="116"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TiMiditySettingsDialog</name>
+    <message>
+        <location filename="../timiditysettingsdialog.ui" line="14"/>
+        <source>TiMidity Plugin Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timiditysettingsdialog.ui" line="39"/>
+        <source>Instrument configuration: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timiditysettingsdialog.ui" line="46"/>
+        <source>Sample rate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timiditysettingsdialog.cpp" line="39"/>
+        <source>44100 Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timiditysettingsdialog.cpp" line="40"/>
+        <source>48000 Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
