0a2d1c4638c10db82145043c82656ae9ba899cf8
trialuser02 <trialuser02>
2026-04-26 14:14:56 +0000

    fixed possible null pointer dereference
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk/qmmp@13094 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/wavpack/decoder_wavpack.cpp
+++ src/plugins/Input/wavpack/decoder_wavpack.cpp
@@ -84,10 +84,10 @@ bool DecoderWavPack::initialize()
             int cue_len = WavpackGetTagItem(m_context, "cuesheet", nullptr, 0);
             if(cue_len > 0)
             {
-                char *value = (char*)malloc(cue_len * 2 + 1);
-                WavpackGetTagItem(m_context, "cuesheet", value, cue_len + 1);
-                m_parser = new CueParser(value);
-                free(value);
+                char *value = new char[cue_len];
+                WavpackGetTagItem(m_context, "cuesheet", value, cue_len);
+                m_parser = new CueParser(value, cue_len);
+                delete [] value;
                 m_parser->setDuration(WavpackGetNumSamples64(m_context) * 1000 / WavpackGetSampleRate(m_context));
                 m_parser->setUrl(u"wvpack"_s, p);
                 m_track = m_path.section(QLatin1Char('#'), -1).toInt();
