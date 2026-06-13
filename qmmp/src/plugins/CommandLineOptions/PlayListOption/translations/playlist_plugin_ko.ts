0565d8b737d549c56dbbc97a7c65719506c5fc8a
trialuser02 <trialuser02>
2026-06-13 11:30:59 +0000

    updated .ts files
    git-svn-id: file:///home/maarten/qmmp-svn-raw/trunk@13195 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/CommandLineOptions/PlayListOption/translations/playlist_plugin_ko.ts
+++ qmmp/src/plugins/CommandLineOptions/PlayListOption/translations/playlist_plugin_ko.ts
@@ -40,76 +40,92 @@
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="40"/>
+        <source>Remove playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="41"/>
         <source>Activate next playlist</source>
         <translation>다음 재생목록 활성화</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="41"/>
+        <location filename="../playlistoption.cpp" line="42"/>
         <source>Activate previous playlist</source>
         <translation>이전 재생목록 활성화</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="42"/>
+        <location filename="../playlistoption.cpp" line="43"/>
         <source>Toggle playlist repeat</source>
         <translation>재생목록 반복 전환</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="43"/>
+        <location filename="../playlistoption.cpp" line="44"/>
         <source>Toggle playlist shuffle</source>
         <translation>재생목록 순서섞기 전환</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="44"/>
+        <location filename="../playlistoption.cpp" line="45"/>
         <source>Show playlist options</source>
         <translation>재생목록 옵션 표시</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="100"/>
+        <location filename="../playlistoption.cpp" line="94"/>
         <source>Arguments:</source>
         <translation>인수:</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="101"/>
+        <location filename="../playlistoption.cpp" line="95"/>
         <source>%1 - index or name of the playlist</source>
         <translation>%1 - 재생목록의 인덱스 또는 이름</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="102"/>
+        <location filename="../playlistoption.cpp" line="96"/>
         <source>%1 - index of the track</source>
         <translation>%1 - 트랙의 인덱스</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="103"/>
+        <location filename="../playlistoption.cpp" line="97"/>
         <source>%1 - name of the new playlist</source>
         <translation>%1 - 새 재생목록의 이름</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="124"/>
-        <location filename="../playlistoption.cpp" line="141"/>
-        <location filename="../playlistoption.cpp" line="166"/>
-        <location filename="../playlistoption.cpp" line="205"/>
+        <location filename="../playlistoption.cpp" line="118"/>
+        <location filename="../playlistoption.cpp" line="135"/>
+        <location filename="../playlistoption.cpp" line="160"/>
+        <location filename="../playlistoption.cpp" line="199"/>
+        <location filename="../playlistoption.cpp" line="213"/>
         <source>Invalid playlist ID</source>
         <translation>올바르지 않은 재생목록 ID</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="148"/>
+        <location filename="../playlistoption.cpp" line="142"/>
         <source>Invalid playlist name</source>
         <translation>올바르지 않은 재생목록 이름</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="153"/>
+        <location filename="../playlistoption.cpp" line="147"/>
         <source>Playlist with name &quot;%1&quot; already exists</source>
         <translation>이름이 &quot;%1&quot;인 재생목록이 이미 존재합니다</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="162"/>
+        <location filename="../playlistoption.cpp" line="156"/>
         <source>Invalid number of arguments</source>
         <translation>인수의 잘못된 번호</translation>
     </message>
     <message>
-        <location filename="../playlistoption.cpp" line="171"/>
+        <location filename="../playlistoption.cpp" line="165"/>
         <source>Invalid track ID</source>
         <translation>올바르지 않은 트랙 ID</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="206"/>
+        <source>Unable to remove last remaining playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="209"/>
+        <source>Missing playlist ID</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
